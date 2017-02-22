320ab977f4fd5416f8a5e55fc6cd44e7f2e14ca9
Paul Warren <paul.warren@emc.com>
2017-02-21 17:47:01 -0800

    Add ReflectionService test

---
+++ spring-content-commons/src/test/java/internal/org/springframework/content/commons/utils/ReflectionServiceTest.java
@@ -0,0 +1,46 @@
+package internal.org.springframework.content.commons.utils;
+
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.BeforeEach;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Context;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Describe;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.It;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.JustBeforeEach;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.runner.RunWith;
+import org.springframework.content.commons.utils.ReflectionService;
+import org.springframework.content.commons.utils.ReflectionServiceImpl;
+import org.springframework.util.ReflectionUtils;
+
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jRunner;
+
+@RunWith(Ginkgo4jRunner.class)
+public class ReflectionServiceTest {
+
+	private ReflectionService reflectionService;
+	
+	// mocks
+	private HelloWorldService service;
+	
+	{
+		Describe("ReflectionService", () -> {
+			Context("invokeMethod",  () -> {
+				BeforeEach(() -> {
+					service = mock(HelloWorldService.class);
+				});
+				JustBeforeEach(() -> {
+					reflectionService = new ReflectionServiceImpl();
+					reflectionService.invokeMethod(ReflectionUtils.findMethod(HelloWorldService.class, "helloWorld"), service, new Object[]{});
+				});
+				It("should invoke the method", () -> {
+					verify(service).helloWorld();
+				});
+			});
+		});
+	}
+	
+	public interface HelloWorldService {
+		void helloWorld();
+	}
+}
