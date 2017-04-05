10d6c80d7c96a1911f9c4c673c1b2e107ab1646e
Paul Warren <paul.warren@emc.com>
2017-04-05 13:43:10 -0700

    Remove unused SearchException

--- spring-content-commons/src/main/java/org/springframework/content/commons/search/SearchException.java
+++
@@ -1,6 +0,0 @@
-package org.springframework.content.commons.search;
-
-
-public class SearchException extends RuntimeException{
-    public SearchException() { super("Missing extension implementation for service org.springframework.content.commons.search.Searchable"); }
-}
