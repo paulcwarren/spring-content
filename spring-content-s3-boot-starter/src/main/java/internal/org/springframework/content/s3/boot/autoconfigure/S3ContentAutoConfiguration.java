fa51512a37b3eacb44e20e3c1f392555fff2a097
Paul Warren <paul.warren@emc.com>
2017-04-05 22:32:40 -0700

    Add s3 configuration to boot starter auto configuration

--- spring-content-s3-boot-starter/src/main/java/internal/org/springframework/content/s3/boot/autoconfigure/S3ContentAutoConfiguration.java
+++ spring-content-s3-boot-starter/src/main/java/internal/org/springframework/content/s3/boot/autoconfigure/S3ContentAutoConfiguration.java
@@ -2,6 +2,8 @@ package internal.org.springframework.content.s3.boot.autoconfigure;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 
+import internal.org.springframework.content.s3.config.S3ContentRepositoryConfiguration;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.cloud.aws.context.config.annotation.ContextResourceLoaderConfiguration;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +11,7 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @ConditionalOnClass(AmazonS3Client.class)
-@Import({ContextResourceLoaderConfiguration.class, S3ContentAutoConfigureRegistrar.class})
+@Import({ContextResourceLoaderConfiguration.class, S3ContentRepositoryConfiguration.class, S3ContentAutoConfigureRegistrar.class})
 public class S3ContentAutoConfiguration {
 
 }
