334c051f9abeee838d467c0ba2032853789d638f
Paul Warren <paul.warren@emc.com>
2017-04-12 12:12:34 -0700

    Remove unused code - in lieu of removing the ContentOeprations iface

--- spring-content-commons/src/main/java/org/springframework/content/commons/operations/AbstractResourceTemplate.java
+++ spring-content-commons/src/main/java/org/springframework/content/commons/operations/AbstractResourceTemplate.java
@@ -1,19 +1,11 @@
 package org.springframework.content.commons.operations;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.UUID;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.content.commons.annotations.ContentId;
-import org.springframework.content.commons.annotations.ContentLength;
-import org.springframework.content.commons.utils.BeanUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.core.io.WritableResource;
 
 public abstract class AbstractResourceTemplate implements ResourceOperations, ContentOperations {
 	
@@ -27,79 +19,18 @@ public abstract class AbstractResourceTemplate implements ResourceOperations, Co
 	
 	@Override
 	public <S> void setContent(S property, InputStream content) {
-		Object contentId = BeanUtils.getFieldWithAnnotation(property, ContentId.class);
-		if (contentId == null) {
-			contentId = UUID.randomUUID();
-			BeanUtils.setFieldWithAnnotation(property, ContentId.class, contentId.toString());
-		}
-
-		Resource resource = this.get(this.getLocation(contentId));
-		OutputStream os = null;
-		try {
-			if (resource instanceof WritableResource) {
-				os = ((WritableResource)resource).getOutputStream();
-				IOUtils.copy(content, os);
-			}
-		} catch (IOException e) {
-			logger.error(String.format("Unexpected error setting content %s", contentId.toString()), e);
-		} finally {
-	        try {
-	            if (os != null) {
-	                os.close();
-	            }
-	        } catch (IOException ioe) {
-	            // ignore
-	        }
-		}
-			
-		try {
-			BeanUtils.setFieldWithAnnotation(property, ContentLength.class, resource.contentLength());
-		} catch (IOException e) {
-			logger.error(String.format("Unexpected error setting content length for content %s", contentId.toString()), e);
-		}
+		// unused
 	}
 
 	@Override
 	public <S> InputStream getContent(S property) {
-		if (property == null)
-			return null;
-		Object contentId = BeanUtils.getFieldWithAnnotation(property, ContentId.class);
-		if (contentId == null)
-			return null;
-
-		Resource resource = this.get(this.getLocation(contentId));
-		try {
-			if (resource.exists()) {
-				return resource.getInputStream();
-			}
-		} catch (IOException e) {
-			logger.error(String.format("Unexpected error getting content %s", contentId.toString()), e);
-		}
-		
+		// unused
 		return null;
 	}
 
 	@Override
 	public <S> void unsetContent(S property) {
-		if (property == null)
-			return;
-		Object contentId = BeanUtils.getFieldWithAnnotation(property, ContentId.class);
-		if (contentId == null)
-			return;
-
-		// delete any existing content object
-		try {
-			Resource resource = this.get(this.getLocation(contentId));
-			if (resource.exists()) {
-				this.delete(resource);
-
-				// reset content fields
-		        BeanUtils.setFieldWithAnnotation(property, ContentId.class, null);
-		        BeanUtils.setFieldWithAnnotation(property, ContentLength.class, 0);
-			}
-		} catch (Exception ase) {
-			logger.error(String.format("Unexpected error unsetting content %s", contentId.toString()), ase);
-		}
+		// unused
 	}
 
     @Override
