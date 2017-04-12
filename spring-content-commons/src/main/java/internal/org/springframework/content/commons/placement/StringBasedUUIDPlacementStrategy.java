b78f0fadfa082bbec9e672401e8d45f856d6deed
Paul Warren <paul.warren@emc.com>
2017-04-11 22:13:00 -0700

    Add tests for string-based uuid placement strategy

--- spring-content-commons/src/main/java/internal/org/springframework/content/commons/placement/StringBasedUUIDPlacementStrategy.java
+++ spring-content-commons/src/main/java/internal/org/springframework/content/commons/placement/StringBasedUUIDPlacementStrategy.java
@@ -3,11 +3,13 @@ package internal.org.springframework.content.commons.placement;
 import java.util.UUID;
 
 import org.springframework.content.commons.placement.PlacementStrategy;
+import org.springframework.util.Assert;
 
 public class StringBasedUUIDPlacementStrategy implements PlacementStrategy<String> {
 
 	@Override
 	public String getLocation(String contentId) {
+		Assert.notNull(contentId);
 		return new UUIDPlacementStrategy().getLocation(UUID.fromString(contentId));
 	}
 
