a41ad0f8af09c5f9cd18dedb5b7acee4aa3198d2
Paul Warren <paul.warren@emc.com>
2017-05-22 22:53:24 -0700

    Correct paths

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'release' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_2d46c2ddc73e_key -iv $encrypted_2d46c2ddc73e_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
-    gpg --fast-import cd/signingkey.asc
+    openssl aes-256-cbc -K $encrypted_2d46c2ddc73e_key -iv $encrypted_2d46c2ddc73e_iv -in signingkey.asc.enc -out signingkey.asc -d
+    gpg --fast-import signingkey.asc
 fi
