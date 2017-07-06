f681f1ec6ca39895a8769c8a985309f76bf62342
Paul Warren <paul.warren@emc.com>
2017-07-05 18:37:22 -0700

    Remove unrequired bean

--- spring-content-rest-boot-starter/src/main/java/internal/org/springframework/content/rest/boot/autoconfigure/HypermediaAutoConfiguration.java
+++ spring-content-rest-boot-starter/src/main/java/internal/org/springframework/content/rest/boot/autoconfigure/HypermediaAutoConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.content.rest.config.HypermediaConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
@@ -14,9 +13,5 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguratio
 @AutoConfigureAfter({ RepositoryRestMvcAutoConfiguration.class })
 @Import(HypermediaConfiguration.class)
 public class HypermediaAutoConfiguration {
-
-	@Bean
-	public Object aBean() {
-		return new Object();
-	}
+	//
 }
