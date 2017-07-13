8983e1cb0cbf2e3454089f01749fabfc65fd906d
Paul Warren <paul.warren@emc.com>
2017-07-12 19:10:23 -0700

    Update build scripts to perform release on github tag builds (test)

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'release' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ -n "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_2d46c2ddc73e_key -iv $encrypted_2d46c2ddc73e_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import codesigning.asc
 fi
