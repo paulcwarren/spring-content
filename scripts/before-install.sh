5afc00a32714adbc1c24d3f59042774f91ac78e9
Paul Warren <paul.warren@emc.com>
2017-07-13 17:58:33 -0700

    travis cli not available try bash export

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,8 +1,10 @@
+echo ${MAVEN_PROFILE}
+
 #!/usr/bin/env bash
 if [ -n "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_2d46c2ddc73e_key -iv $encrypted_2d46c2ddc73e_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import codesigning.asc
-    travis env set MAVEN_PROFILE release
+    export MAVEN_PROFILE=release
 else
-    travis env set MAVEN_PROFILE master
+    export MAVEN_PROFILE=master
 fi
