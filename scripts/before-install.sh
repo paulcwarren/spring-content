a354529ecfd56680b88a5e44cbcb7d05e7fe8f7b
Paul Warren <paul.warren@emc.com>
2017-07-13 17:35:40 -0700

    Explicitly set maven profile based on whether this is a tag build, or not

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -2,4 +2,7 @@
 if [ -n "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_2d46c2ddc73e_key -iv $encrypted_2d46c2ddc73e_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import codesigning.asc
+    export MAVEN_PROFILE=release
+else
+    export MAVEN_PROFILE=master
 fi
