ba2ba48d7bfe8b8422b0224914a96841b5307023
Paul Warren <paul.warren@emc.com>
2017-07-13 22:35:02 -0700

    Always import gpg signature so we can deploy SNAPSHOTs

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 echo ${MAVEN_PROFILE}
 
-#!/usr/bin/env bash
-if [ -n "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_2d46c2ddc73e_key -iv $encrypted_2d46c2ddc73e_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import codesigning.asc
+openssl aes-256-cbc -K $encrypted_2d46c2ddc73e_key -iv $encrypted_2d46c2ddc73e_iv -in codesigning.asc.enc -out codesigning.asc -d
+gpg --fast-import codesigning.asc
+
+if [ -n "$TRAVIS_TAG" ]; then
     export MAVEN_PROFILE=release
 else
     export MAVEN_PROFILE=master
