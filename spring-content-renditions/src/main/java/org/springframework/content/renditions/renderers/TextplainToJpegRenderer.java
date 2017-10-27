0fcceb7ce5aea863aef674604d71aac4666f8b11
Paul Warren <paul.warren@emc.com>
2017-10-27 16:59:19 -0700

    Initial implementation of text/plain to image/jpeg renderer

---
+++ spring-content-renditions/src/main/java/org/springframework/content/renditions/renderers/TextplainToJpegRenderer.java
@@ -0,0 +1,158 @@
+package org.springframework.content.renditions.renderers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.content.commons.renditions.RenditionProvider;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.LineBreakMeasurer;
+import java.awt.font.TextAttribute;
+import java.awt.font.TextLayout;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.text.AttributedCharacterIterator;
+import java.text.AttributedString;
+import java.util.ArrayList;
+
+@Service
+public class TextplainToJpegRenderer implements RenditionProvider {
+
+    private static Log logger = LogFactory.getLog(WordToJpegRenderer.class);
+
+    private static int width = 272;
+    private static int margin = 5;
+    private static String fontName = "Courier New";
+    private static int fontSize = 12;
+
+    private boolean wrapText = false;
+
+    public TextplainToJpegRenderer(boolean wrapText) {
+        this.wrapText = wrapText;
+    }
+
+    @Override
+    public String consumes() {
+        return "text/plain";
+    }
+
+    @Override
+    public String[] produces() {
+        return new String[] {"image/jpg"};
+    }
+
+    @Override
+    public InputStream convert(InputStream fromInputSource, String toMimeType) {
+
+        Assert.notNull(fromInputSource, "input source must not be null");
+
+        Font font = null;
+        try {
+            font = new Font(fontName, Font.PLAIN, (int) fontSize);
+        } catch (Exception e) {
+            logger.error("Error creating font: " + e.getMessage());
+            return null;
+        }
+
+        BufferedImage tempBuffer = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = tempBuffer.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setFont(font);
+
+        FontRenderContext fc = g.getFontRenderContext();
+        Rectangle2D bounds = font.getStringBounds("Random Text", fc);
+        int lineHeight = (int)bounds.getHeight();
+
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(fromInputSource));
+        } catch (Exception e) {
+            logger.error("Error opening input stream", e);
+            return null;
+        }
+
+        ArrayList images = new ArrayList();
+
+        int lineCnt = margin + margin;
+
+        while (true) {
+            String line;
+
+            try {
+                line = reader.readLine();
+            } catch (IOException ignore) {
+                break;
+            }
+
+            if (line == null) { // EOF
+                break;
+            }
+
+            if ("".equals(line)) { // Empty line
+                line = " ";
+            }
+
+            AttributedString attribString = new AttributedString(line);
+            attribString.addAttribute(TextAttribute.BACKGROUND, Color.WHITE, 0, line.length());
+            attribString.addAttribute(TextAttribute.FOREGROUND, Color.BLACK, 0, line.length());
+            attribString.addAttribute(TextAttribute.FONT, font, 0, line.length());
+
+            AttributedCharacterIterator aci = attribString.getIterator();
+            LineBreakMeasurer lbm = new LineBreakMeasurer(aci, fc);
+
+            while (lbm.getPosition() < line.length()) {
+                BufferedImage lineBuffer = new BufferedImage(width, lineHeight, BufferedImage.TYPE_INT_ARGB);
+
+                Graphics2D g1 = lineBuffer.createGraphics();
+                g1.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                TextLayout layout = lbm.nextLayout(width - margin);
+
+                int y = (int) layout.getAscent();
+
+                layout.draw(g1, margin, y);
+                images.add(lineBuffer);
+                lineCnt += lineHeight;
+
+                if (lineCnt + lineHeight > 480 || !wrapText) {
+                    break;
+//                    images.clear();
+//                    lineCnt = margin + margin;
+                }
+            }
+        }
+
+        if (lineCnt != 0) {
+            try {
+                saveImage(images, lineHeight, "/tmp/textToJpeg.jpeg");
+                return new FileInputStream("/tmp/textToJpeg.jpeg");
+            } catch (IOException e) {
+                logger.error("Error writing image", e);
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    private void saveImage(ArrayList<BufferedImage> images, int lineHeight, String fileName) throws IOException {
+        BufferedImage buffer = new BufferedImage(272, 480, BufferedImage.TYPE_INT_RGB);
+
+        Graphics2D g = buffer.createGraphics();
+        g.setBackground(Color.WHITE);
+        g.clearRect(0, 0, 272, 480);
+
+        for (int i = 0; i < images.size(); i++) {
+            g.drawImage((BufferedImage) images.get(i), 0, margin + (i * lineHeight), null);
+        }
+
+        OutputStream out = new FileOutputStream(new File(fileName.toString()));
+        ImageIO.write(buffer, "jpg", out);
+        out.close();
+    }
+}
---
+++ spring-content-renditions/src/test/java/org/springframework/content/renditions/renderers/TextplainToJpegRendererTest.java
@@ -0,0 +1,111 @@
+package org.springframework.content.renditions.renderers;
+
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jConfiguration;
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jRunner;
+import org.apache.commons.io.IOUtils;
+import org.junit.runner.RunWith;
+import org.springframework.content.commons.renditions.RenditionProvider;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(Ginkgo4jRunner.class)
+@Ginkgo4jConfiguration(threads=1)
+public class TextplainToJpegRendererTest {
+
+    private boolean wrapText = false;
+    private RenditionProvider renderer;
+
+    private InputStream input, result;
+    private String mimeType;
+
+    private Exception e;
+
+    {
+        Describe("TextplainToJpegRenderer", () -> {
+            JustBeforeEach(() -> {
+                renderer = new TextplainToJpegRenderer(wrapText);
+            });
+            Context("#consumes", () -> {
+                It("should return text/plain", () -> {
+                    assertThat(renderer.consumes(), is("text/plain"));
+                });
+            });
+            Context("#produces", () -> {
+                It("should return jpeg mimetype", () -> {
+                    assertThat(renderer.produces(), hasItemInArray("image/jpg"));
+                });
+            });
+            Context("#convert", () -> {
+                JustBeforeEach(() -> {
+                    try {
+                        result = renderer.convert(input, mimeType);
+                    } catch (Exception e) {
+                        this.e = e;
+                    }
+                });
+                Context("given a plain/text input", () -> {
+                    Context("given a single-line input", () -> {
+                        BeforeEach(()-> {
+                            input = new ByteArrayInputStream("Hello Spring Content World!".getBytes());
+                        });
+                        It("should produce the correct image", () -> {
+                            assertThat(IOUtils.contentEquals(this.getClass().getResourceAsStream("/textplaintorenderer/single-line.jpeg"), result), is(true));
+                        });
+                    });
+                    Context("given a multi-line input", () -> {
+                        BeforeEach(()-> {
+                            input = new ByteArrayInputStream("Hello\nSpring\n\nContent\n\n\nWorld!".getBytes());
+                        });
+                        It("should produce the correct image", () -> {
+                            assertThat(IOUtils.contentEquals(this.getClass().getResourceAsStream("/textplaintorenderer/multi-line.jpeg"), result), is(true));
+                        });
+                    });
+                    Context("given a long line and wrapping", () -> {
+                        BeforeEach(()-> {
+                            wrapText = true;
+                            input = new ByteArrayInputStream("Hello Spring Content World!  This is a really long line that we expect to wrap".getBytes());
+                        });
+                        It("should produce the correct image", () -> {
+                            assertThat(IOUtils.contentEquals(this.getClass().getResourceAsStream("/textplaintorenderer/wrapped-line.jpeg"), result), is(true));
+                        });
+                    });
+                    Context("given a long line and no wrapping", () -> {
+                        BeforeEach(()-> {
+                            input = new ByteArrayInputStream("Hello Spring Content World!  This is a really long line that we expect to wrap".getBytes());
+                        });
+                        It("should produce the correct image", () -> {
+                            assertThat(IOUtils.contentEquals(this.getClass().getResourceAsStream("/textplaintorenderer/overflowed-line.jpeg"), result), is(true));
+                        });
+                    });
+                    Context("given a line file will overflow the image size", () -> {
+                        BeforeEach(()-> {
+                            input = new ByteArrayInputStream("Hello\n\nSpring\n\nContent\n\nWorld!\n\n\nThis\n\nis\n\na\n\nreally\n\nreally\n\nreally\n\nreally\n\nreally\n\nlong\n\nfile\n\nthat\n\nwill\n\noverflow\n\nthe\n\nimage".getBytes());
+                        });
+                        It("should produce the correct image", () -> {
+                            assertThat(IOUtils.contentEquals(this.getClass().getResourceAsStream("/textplaintorenderer/overflowed-image.jpeg"), result), is(true));
+                        });
+                    });
+                });
+                Context("when the input stream is not a valid word file", () -> {
+                    BeforeEach(()-> {
+                        input = this.getClass().getResourceAsStream("/sample-docx.docx");
+                    });
+                    It("should not error", () -> {
+                        assertThat(e, is(nullValue()));
+                    });
+                });
+                Context("given a null input stream", () -> {
+                    It("should get the embedded thumbnail from the XWPFDocument's properties", () -> {
+                        assertThat(e, is(not(nullValue())));
+                    });
+                });
+            });
+        });
+    }
+}
