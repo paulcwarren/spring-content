90a3cff41bad93689c15595e989a6d54f16b95c6
Paul Warren <paul.warren@emc.com>
2018-04-11 09:19:24 -0700

    Add support for mutiple buckets through an S3ContentId - https://github.com/paulcwarren/spring-content/issues/22

--- spring-content-s3/src/main/java/org/springframework/content/s3/S3ContentId.java
+++ spring-content-s3/src/main/java/org/springframework/content/s3/S3ContentId.java
@@ -1,4 +1,55 @@
-package org.springframework.content.s3.config.id;
+package org.springframework.content.s3;
 
-public class S3ContentId {
+import java.io.Serializable;
+
+public final class S3ContentId implements Serializable {
+    private String bucket;
+    private String objectId;
+
+    public S3ContentId(String bucket, String objectId) {
+        this.bucket = bucket;
+        this.objectId = objectId;
+    }
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
+    public String getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
+    @Override
+    public String toString() {
+        return "S3ContentId{" +
+                "bucket='" + bucket + '\'' +
+                ", objectId='" + objectId + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        S3ContentId that = (S3ContentId) o;
+
+        if (bucket != null ? !bucket.equals(that.bucket) : that.bucket != null) return false;
+        return objectId != null ? objectId.equals(that.objectId) : that.objectId == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = bucket != null ? bucket.hashCode() : 0;
+        result = 31 * result + (objectId != null ? objectId.hashCode() : 0);
+        return result;
+    }
 }
