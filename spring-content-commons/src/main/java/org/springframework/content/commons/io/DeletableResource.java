0270c4d4648642d5d4465b6852cab808c42ab83d
Paul Warren <paul.warren@emc.com>
2018-05-14 19:52:17 -0700

    Add javadoc to explain delete semantics

--- spring-content-commons/src/main/java/org/springframework/content/commons/io/DeletableResource.java
+++ spring-content-commons/src/main/java/org/springframework/content/commons/io/DeletableResource.java
@@ -3,5 +3,13 @@ package org.springframework.content.commons.io;
 import org.springframework.core.io.Resource;
 
 public interface DeletableResource extends Resource {
+
+	/**
+	 * Deletes the resource.
+	 * <br><br>
+	 * Returns true if the deletion was successful, otherwise false.  If the operation returns true then the resource
+	 * handle itself must be considered unreliable and should be discarded.
+	 */
 	void delete();
+
 }
