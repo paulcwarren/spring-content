ff91903767053e27e5f26e017d1cbdb9cf413adc
Paul Warren <paul.warren@emc.com>
2018-06-28 23:29:07 -0700

    Add javadoc to AssociativeStore and Store interfaces

--- spring-content-commons/src/main/java/org/springframework/content/commons/repository/AssociativeStore.java
+++ spring-content-commons/src/main/java/org/springframework/content/commons/repository/AssociativeStore.java
@@ -6,10 +6,31 @@ import java.io.Serializable;
 
 public interface AssociativeStore<S, SID extends Serializable> extends Store<SID> {
 
+	/**
+	 * Returns the resource associated with the given entity, or null if no association exists
+	 *
+	 * @param entity
+	 * 			the entity associated with resource
+	 * @return	resource
+	 */
 	Resource getResource(S entity);
 
+	/**
+	 * Associates the resource (identified by id) with entity.
+	 *
+	 * @param entity
+	 * 			the target of the association
+	 * @param id
+	 * 			the id of the resource to be associated
+	 */
 	void associate(S entity, SID id);
 
+	/**
+	 * Unassociates the resource from entity
+	 *
+	 * @param entity
+	 * 			the target of the unassociation
+	 */
 	void unassociate(S entity);
 
 }
--- spring-content-commons/src/main/java/org/springframework/content/commons/repository/Store.java
+++ spring-content-commons/src/main/java/org/springframework/content/commons/repository/Store.java
@@ -5,7 +5,13 @@ import java.io.Serializable;
 import org.springframework.core.io.Resource;
 
 public interface Store<SID extends Serializable> {
-
+	/**
+	 * Returns a resource handle for the specified id.
+	 *
+	 * @param id
+	 * 			the id of the resource
+	 * @return
+	 * 			resource
+	 */
 	Resource getResource(SID id);
-
 }
