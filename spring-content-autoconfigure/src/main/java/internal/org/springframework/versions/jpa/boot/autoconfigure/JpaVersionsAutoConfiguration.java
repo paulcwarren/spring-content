1ad64a75cf8efa63176c840edc09828e6e69eedb
Paul Warren <paul.warren@emc.com>
2018-12-12 20:27:47 -0800

    Add boot starter for spring-versions-jpa

---
+++ spring-content-autoconfigure/src/main/java/internal/org/springframework/versions/jpa/boot/autoconfigure/JpaVersionsAutoConfiguration.java
@@ -0,0 +1,38 @@
+package internal.org.springframework.versions.jpa.boot.autoconfigure;
+
+import internal.org.springframework.content.jpa.config.JpaStoresRegistrar;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.versions.jpa.config.JpaLockingAndVersioningConfig;
+
+import javax.sql.DataSource;
+
+@Configuration
+@AutoConfigureAfter(DataSourceAutoConfiguration.class)
+@ConditionalOnClass({ DataSource.class })
+@Import({ JpaLockingAndVersioningConfig.class })
+@EnableConfigurationProperties(JpaVersionsProperties.class)
+public class JpaVersionsAutoConfiguration {
+
+    private final JpaVersionsProperties properties;
+
+    public JpaVersionsAutoConfiguration(JpaVersionsProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnBean(DataSource.class)
+    public JpaVersionsDatabaseInitializer databaseInitializer(DataSource dataSource,
+                                                             ResourceLoader resourceLoader) {
+        return new JpaVersionsDatabaseInitializer(dataSource, resourceLoader, this.properties);
+    }
+}
