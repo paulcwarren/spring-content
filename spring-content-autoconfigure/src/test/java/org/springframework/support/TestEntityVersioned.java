e39a88e52a922dab5338ff7d8bf8fd4c38429e1c
Paul Warren <paul.warren@emc.com>
2019-04-09 22:42:46 -0700

    Make test entities for versioned repositories implement an ancestorId field
    - required after the working copy refactor

--- spring-content-autoconfigure/src/test/java/org/springframework/support/TestEntityVersioned.java
+++ spring-content-autoconfigure/src/test/java/org/springframework/support/TestEntityVersioned.java
@@ -1,12 +1,13 @@
 package org.springframework.support;
 
-import org.springframework.versions.AncestorRootId;
-import org.springframework.versions.SuccessorId;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.springframework.versions.AncestorId;
+import org.springframework.versions.AncestorRootId;
+import org.springframework.versions.SuccessorId;
 
 @Entity
 public class TestEntityVersioned {
@@ -14,5 +15,6 @@ public class TestEntityVersioned {
 
     // these are required to satisfy the
     @AncestorRootId private String ancestralRootId;
+    @AncestorId private String ancestorId;
     @SuccessorId private String successorId;
 }
