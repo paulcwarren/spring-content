a395b6e43574de98364affa8e902ea5c6094d538
Paul Warren <paul.warren@emc.com>
2019-04-19 16:12:13 -0700

    Add script for running clean test build on all projects, or just one depending on command line args

---
+++ scripts/test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -eu
+
+function usage() {
+  cat <<EOT
+Usage: $(basename "$0") <options>
+Options:
+  -s  Test project identified by suffix (\`-s .\` test all) 
+EOT
+}
+
+function test() {
+  
+  if [[ -z "${1}" ]]; then
+		BUILD_TYPE=dev mvn clean test     
+  elif [[ "${1}" == "all" ]]; then
+		BUILD_TYPE=dev mvn clean test     
+  else
+		BUILD_TYPE=dev mvn -pl "spring-content-${1}" clean test     
+  fi
+
+}
+
+function main() {
+  local o
+
+  while getopts a:s:h: o; do
+      case $o in
+        a)
+          test
+          ;;
+        s)
+          test "${OPTARG}"
+          ;;
+        *)
+          echo "Invalid flag: ${o}"
+          usage
+          exit 1
+      esac
+    done
+
+   shift $((OPTIND-1))
+}
+
+main "$@"
