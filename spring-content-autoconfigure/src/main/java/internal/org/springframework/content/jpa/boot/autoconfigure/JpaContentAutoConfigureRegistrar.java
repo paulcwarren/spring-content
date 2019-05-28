6296d8c15e657fb7095cbf03a4920f3520f5f728
Paul Warren <paul.warren@emc.com>
2019-05-28 07:34:28 -0700

    Refactor store autoconfiguration onto the new store extension mechanism

--- spring-content-autoconfigure/src/main/java/internal/org/springframework/content/jpa/boot/autoconfigure/JpaContentAutoConfigureRegistrar.java
+++ spring-content-autoconfigure/src/main/java/internal/org/springframework/content/jpa/boot/autoconfigure/JpaContentAutoConfigureRegistrar.java
@@ -1,19 +1,17 @@
 package internal.org.springframework.content.jpa.boot.autoconfigure;
 
+import java.util.Set;
+
 import internal.org.springframework.content.commons.utils.StoreUtils;
 import internal.org.springframework.content.jpa.config.JpaStoresRegistrar;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
-import org.springframework.content.commons.config.AbstractStoreBeanDefinitionRegistrar;
 import org.springframework.content.jpa.config.EnableJpaStores;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.core.type.StandardAnnotationMetadata;
-
-import java.util.Set;
 
 public class JpaContentAutoConfigureRegistrar extends JpaStoresRegistrar {
 
@@ -28,24 +26,9 @@ public class JpaContentAutoConfigureRegistrar extends JpaStoresRegistrar {
 
 		String[] basePackages = this.getBasePackages();
 
-		Set<GenericBeanDefinition> definitions = StoreUtils
-				.getStoreCandidates(this.getResourceLoader(), basePackages);
+		Set<GenericBeanDefinition> definitions = StoreUtils.getStoreCandidates(this.getResourceLoader(), basePackages);
 
-		for (BeanDefinition definition : definitions) {
-
-			String factoryBeanName = StoreUtils.getStoreFactoryBeanName(attributes);
-
-			BeanDefinitionBuilder builder = BeanDefinitionBuilder
-					.rootBeanDefinition(factoryBeanName);
-
-			builder.getRawBeanDefinition().setSource(importingClassMetadata);
-			builder.addPropertyValue(
-					AbstractStoreBeanDefinitionRegistrar.STORE_INTERFACE_PROPERTY,
-					definition.getBeanClassName());
-
-			registry.registerBeanDefinition(StoreUtils.getStoreBeanName(definition),
-					builder.getBeanDefinition());
-		}
+		this.buildAndRegisterDefinitions(importingClassMetadata, registry, attributes, basePackages, definitions);
 	}
 
 	protected String[] getBasePackages() {
