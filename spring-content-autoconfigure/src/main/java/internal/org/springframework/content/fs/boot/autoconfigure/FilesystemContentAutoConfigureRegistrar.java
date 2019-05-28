428dd6c1edc15d6062650480475185df2357915f
Paul Warren <paul.warren@emc.com>
2019-05-28 07:34:28 -0700

    Refactor store autoconfiguration onto the new store extension mechanism

--- spring-content-autoconfigure/src/main/java/internal/org/springframework/content/fs/boot/autoconfigure/FilesystemContentAutoConfigureRegistrar.java
+++ spring-content-autoconfigure/src/main/java/internal/org/springframework/content/fs/boot/autoconfigure/FilesystemContentAutoConfigureRegistrar.java
@@ -2,51 +2,30 @@ package internal.org.springframework.content.fs.boot.autoconfigure;
 
 import java.util.Set;
 
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import internal.org.springframework.content.commons.utils.StoreUtils;
+import internal.org.springframework.content.fs.config.FilesystemStoreRegistrar;
+
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
-import org.springframework.content.commons.config.AbstractStoreBeanDefinitionRegistrar;
 import org.springframework.content.fs.config.EnableFilesystemStores;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.core.type.StandardAnnotationMetadata;
 
-import internal.org.springframework.content.commons.utils.StoreUtils;
-import internal.org.springframework.content.fs.config.FilesystemStoreRegistrar;
-
 public class FilesystemContentAutoConfigureRegistrar extends FilesystemStoreRegistrar {
 
 	@Override
-	protected void registerContentStoreBeanDefinitions(
-			AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+	protected void registerContentStoreBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
 
-		AnnotationMetadata metadata = new StandardAnnotationMetadata(
-				EnableFilesystemContentAutoConfiguration.class);
-		AnnotationAttributes attributes = new AnnotationAttributes(
-				metadata.getAnnotationAttributes(this.getAnnotation().getName()));
+		AnnotationMetadata metadata = new StandardAnnotationMetadata(EnableFilesystemContentAutoConfiguration.class);
+		AnnotationAttributes attributes = new AnnotationAttributes(metadata.getAnnotationAttributes(this.getAnnotation().getName()));
 
 		String[] basePackages = this.getBasePackages();
 
-		Set<GenericBeanDefinition> definitions = StoreUtils
-				.getStoreCandidates(this.getResourceLoader(), basePackages);
+		Set<GenericBeanDefinition> definitions = StoreUtils.getStoreCandidates(this.getResourceLoader(), basePackages);
 
-		for (BeanDefinition definition : definitions) {
-
-			String factoryBeanName = StoreUtils.getStoreFactoryBeanName(attributes);
-
-			BeanDefinitionBuilder builder = BeanDefinitionBuilder
-					.rootBeanDefinition(factoryBeanName);
-
-			builder.getRawBeanDefinition().setSource(importingClassMetadata);
-			builder.addPropertyValue(
-					AbstractStoreBeanDefinitionRegistrar.STORE_INTERFACE_PROPERTY,
-					definition.getBeanClassName());
-
-			registry.registerBeanDefinition(StoreUtils.getStoreBeanName(definition),
-					builder.getBeanDefinition());
-		}
+		this.buildAndRegisterDefinitions(importingClassMetadata, registry, attributes, basePackages, definitions);
 	}
 
 	protected String[] getBasePackages() {
--- spring-content-autoconfigure/src/main/java/internal/org/springframework/content/mongo/boot/autoconfigure/MongoContentAutoConfigureRegistrar.java
+++ spring-content-autoconfigure/src/main/java/internal/org/springframework/content/mongo/boot/autoconfigure/MongoContentAutoConfigureRegistrar.java
@@ -2,19 +2,16 @@ package internal.org.springframework.content.mongo.boot.autoconfigure;
 
 import java.util.Set;
 
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import internal.org.springframework.content.commons.utils.StoreUtils;
+import internal.org.springframework.content.mongo.config.MongoContentStoresRegistrar;
+
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
-import org.springframework.content.commons.config.AbstractStoreBeanDefinitionRegistrar;
 import org.springframework.content.mongo.config.EnableMongoStores;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.core.type.StandardAnnotationMetadata;
-
-import internal.org.springframework.content.commons.utils.StoreUtils;
-import internal.org.springframework.content.mongo.config.MongoContentStoresRegistrar;
 
 public class MongoContentAutoConfigureRegistrar extends MongoContentStoresRegistrar {
 
@@ -29,24 +26,9 @@ public class MongoContentAutoConfigureRegistrar extends MongoContentStoresRegist
 
 		String[] basePackages = this.getBasePackages();
 
-		Set<GenericBeanDefinition> definitions = StoreUtils
-				.getStoreCandidates(this.getResourceLoader(), basePackages);
+		Set<GenericBeanDefinition> definitions = StoreUtils.getStoreCandidates(this.getResourceLoader(), basePackages);
 
-		for (BeanDefinition definition : definitions) {
-
-			String factoryBeanName = StoreUtils.getStoreFactoryBeanName(attributes);
-
-			BeanDefinitionBuilder builder = BeanDefinitionBuilder
-					.rootBeanDefinition(factoryBeanName);
-
-			builder.getRawBeanDefinition().setSource(importingClassMetadata);
-			builder.addPropertyValue(
-					AbstractStoreBeanDefinitionRegistrar.STORE_INTERFACE_PROPERTY,
-					definition.getBeanClassName());
-
-			registry.registerBeanDefinition(StoreUtils.getStoreBeanName(definition),
-					builder.getBeanDefinition());
-		}
+		this.buildAndRegisterDefinitions(importingClassMetadata, registry, attributes, basePackages, definitions);
 	}
 
 	protected String[] getBasePackages() {
