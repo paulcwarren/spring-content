24dec5e8c483171176975069a266c22f7e319cb3
Paul Warren <paul.warren@emc.com>
2019-05-30 08:58:18 -0700

    Implement Searchable search
    - add integration test

---
+++ spring-content-elasticsearch/src/test/java/org/springframework/content/elasticsearch/ElasticsearchConfig.java
@@ -0,0 +1,116 @@
+package org.springframework.content.elasticsearch;
+
+import javax.sql.DataSource;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.content.jpa.config.EnableJpaStores;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.io.Resource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+
+// TODO: Implement @EnableFulltextIndexingElastic
+//@EnableFullTextSolrIndexing
+@Import(FullTextIndexingElasticConfig.class)
+//
+
+@EnableJpaRepositories(considerNestedRepositories = true)
+@EnableJpaStores
+public class ElasticsearchConfig {
+
+//    @Bean
+//    public EmbeddedElastic embeddedElastic() throws IOException, InterruptedException {
+//        return EmbeddedElastic.builder()
+//                .withElasticVersion("6.4.3")
+//                .withSetting(CLUSTER_NAME, "testCluster")
+//                .withSetting("discovery.zen.ping_timeout", 0)
+//                .withPlugin("ingest-attachment")
+//                .withEsJavaOpts("-Xms128m -Xmx512m")
+//                .withStartTimeout(1, MINUTES)
+//                .build()
+//                .start();
+//    }
+//
+//    @PreDestroy
+//    public void tearDown() throws IOException, InterruptedException {
+//        embeddedElastic().stop();
+//    }
+
+    @Bean
+    public ConversionService conversionService() {
+        return new DefaultFormattingConversionService();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder.setType(EmbeddedDatabaseType.H2).build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabase(Database.H2);
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("org.springframework.content.elasticsearch");
+        factory.setDataSource(dataSource());
+
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return txManager;
+    }
+
+    @Bean
+    public RestHighLevelClient client() {
+        return new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+//        return new RestHighLevelClient(RestClient.builder(new HttpHost("search-test2-tnipwmdwedcr6d2rmale5bjp7u.eu-west-2.es.amazonaws.com")));
+    }
+
+    @Value("/org/springframework/content/jpa/schema-drop-hsqldb.sql")
+    private Resource dropReopsitoryTables;
+
+    @Value("/org/springframework/content/jpa/schema-hsqldb.sql")
+    private Resource dataReopsitorySchema;
+
+    @Bean
+    DataSourceInitializer datasourceInitializer(DataSource dataSource) {
+        ResourceDatabasePopulator databasePopulator =
+                new ResourceDatabasePopulator();
+
+        databasePopulator.addScript(dropReopsitoryTables);
+        databasePopulator.addScript(dataReopsitorySchema);
+        databasePopulator.setIgnoreFailedDrops(true);
+
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator);
+
+        return initializer;
+    }
+}
--- spring-content-elasticsearch/src/test/java/org/springframework/content/elasticsearch/ElasticsearchTest.java
+++ spring-content-elasticsearch/src/test/java/org/springframework/content/elasticsearch/ElasticsearchTest.java
@@ -1,37 +1,30 @@
 package org.springframework.content.elasticsearch;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.UUID;
-import java.util.function.Supplier;
+import java.time.Duration;
 
-import com.github.paulcwarren.ginkgo4j.Ginkgo4jRunner;
-import org.apache.http.HttpHost;
-import org.elasticsearch.action.DocWriteResponse;
-import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.delete.DeleteResponse;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jSpringRunner;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.ingest.DeletePipelineRequest;
-import org.elasticsearch.action.ingest.GetPipelineRequest;
-import org.elasticsearch.action.ingest.GetPipelineResponse;
-import org.elasticsearch.action.ingest.PutPipelineRequest;
-import org.elasticsearch.action.ingest.WritePipelineResponse;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.common.bytes.BytesArray;
-import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.node.Node;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import pl.allegro.tech.embeddedelasticsearch.EmbeddedElastic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.content.commons.annotations.ContentId;
+import org.springframework.content.commons.repository.ContentStore;
+import org.springframework.content.commons.search.Searchable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.test.context.ContextConfiguration;
 
 import static com.github.grantwest.eventually.EventuallyLambdaMatcher.eventuallyEval;
 import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.AfterEach;
@@ -39,250 +32,135 @@ import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.BeforeEach;
 import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Context;
 import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Describe;
 import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.It;
-import static java.util.concurrent.TimeUnit.MINUTES;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static pl.allegro.tech.embeddedelasticsearch.PopularProperties.CLUSTER_NAME;
 
-@RunWith(Ginkgo4jRunner.class)
+@RunWith(Ginkgo4jSpringRunner.class)
 //@Ginkgo4jConfiguration(threads=1)
+@ContextConfiguration(classes = {EmbeddedElasticConfig.class, ElasticsearchConfig.class})
 public class ElasticsearchTest {
 
-	private static final String HTTP_BASE_URL = "http://localhost";
-	private static final String HTTP_PORT = "9205";
-	private static final String HTTP_TRANSPORT_PORT = "9305";
-	private static final String elasticSearchBaseUrl = HTTP_BASE_URL + ":" + HTTP_PORT;
-	private static final String ES_WORKING_DIR = "target/es";
-	private static Node node;
+	@Autowired
+	private DocumentRepository repo;
 
-	private static EmbeddedElastic embeddedElastic = null;
-	private static Integer refCount = 0;
+	@Autowired
+	private DocumentContentStore store;
 
-	private RestHighLevelClient client = null;
+	@Autowired
+	private EmbeddedElastic server;
 
-	private WritePipelineResponse wpr = null;
+	@Autowired
+	private RestHighLevelClient client;
 
-	private String id, id2 = null;
-
-	private String attachment;
-
-	private boolean embeddedServer = true;
+	private Document doc1, doc2;
+	private String id1, id2 = null;
 
 	{
-		Describe("ElasticsearchIndexer", () -> {
+		Describe("Elasticsearch Integration", () -> {
 
-			BeforeEach(() -> {
+			Context("given some documents", () -> {
 
-				if (embeddedServer) {
-					System.out.println("Thread " + Thread.currentThread().getId() + " waiting");
-					synchronized (refCount) {
-						System.out.println("Thread " + Thread.currentThread().getId() + " got lock");
-						if (refCount == 0) {
-							System.out.println("Thread " + Thread.currentThread().getId() + " deploying elasticsearch");
-							embeddedElastic = EmbeddedElastic.builder()
-									.withElasticVersion("6.4.3")
-									.withSetting(CLUSTER_NAME, "testCluster")
-									.withSetting("discovery.zen.ping_timeout", 0)
-									.withPlugin("ingest-attachment")
-									.withEsJavaOpts("-Xms128m -Xmx512m")
-									.withStartTimeout(1, MINUTES)
-									.build()
-									.start();
-						}
-						else {
-							System.out.println("Thread " + Thread.currentThread().getId() + " skipping elasticsearch");
-						}
-						refCount++;
-						System.out.println("Thread " + Thread.currentThread()
-								.getId() + " incremented refcount: " + refCount);
-					}
-					client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
-				} else {
-					client = new RestHighLevelClient(RestClient.builder(new HttpHost("search-test2-tnipwmdwedcr6d2rmale5bjp7u.eu-west-2.es.amazonaws.com")));
-				}
-
-				attachment = "attachment_" + UUID.randomUUID().toString();
-			});
-
-			AfterEach(() -> {
-				if (embeddedServer) {
-					synchronized (refCount) {
-						refCount--;
-						System.out.println("Thread " + Thread.currentThread()
-								.getId() + " decrementing refcount: " + refCount);
-						if (refCount == 0 && embeddedElastic != null) {
-							System.out.println("Thread " + Thread.currentThread().getId() + " stopping elasticsearch");
-							embeddedElastic.stop();
-						}
-						else {
-							System.out.println("Thread " + Thread.currentThread()
-									.getId() + " skipping elasticsearch stop");
-						}
-					}
-				}
-			});
-
-			It("the attachment pipeline should not exist", () -> {
-				GetPipelineRequest getRequest = new GetPipelineRequest(attachment);
-				GetPipelineResponse res = client.ingest().getPipeline(getRequest, RequestOptions.DEFAULT);
-				assertThat(res.isFound(), is(false));
-			});
-
-			Context("when the attachment pipeline is created", () -> {
 				BeforeEach(() -> {
-					String source = "{\"description\":\"Extract attachment information encoded in Base64 with UTF-8 charset\"," +
-							"\"processors\":[{\"attachment\":{\"field\":\"data\"}}]}";
-					PutPipelineRequest put = new PutPipelineRequest(attachment,
-							new BytesArray(source.getBytes(StandardCharsets.UTF_8)),
-							XContentType.JSON);
-					wpr = client.ingest().putPipeline(put, RequestOptions.DEFAULT);
+					doc1 = new Document();
+					doc1.setTitle("doc 1");
+					doc1.setAuthor("author@email.com");
+					store.setContent(doc1, this.getClass().getResourceAsStream("/one.docx"));
+					doc1 = repo.save(doc1);
 
-					assertThat(eval(() -> {
-						GetPipelineRequest gpr = new GetPipelineRequest(attachment);
-						GetPipelineResponse gpres = null;
-						gpres = client.ingest().getPipeline(gpr, RequestOptions.DEFAULT);
-						return gpres.isFound();
-					}), eventuallyEval(is(true)));
-
+					doc2 = new Document();
+					doc2.setTitle("doc 2");
+					doc2.setAuthor("author@email.com");
+					store.setContent(doc2, this.getClass().getResourceAsStream("/two.rtf"));
+					doc2 = repo.save(doc2);
 				});
 
 				AfterEach(() -> {
-					System.out.println("deleting attachment pipeline");
-					DeletePipelineRequest del = new DeletePipelineRequest(attachment);
-					wpr = client.ingest().deletePipeline(del, RequestOptions.DEFAULT);
-					assertThat(wpr.isAcknowledged(), is(true));
+					store.unsetContent(doc1);
+					repo.delete(doc1);
+
+					store.unsetContent(doc2);
+					repo.delete(doc2);
 				});
 
-				It("should succeed", () -> {
-					assertThat(wpr.isAcknowledged(), is(true));
+				It("should index the documents", () -> {
+					GetRequest req = new GetRequest("spring-content-fulltext-index", doc1.getClass().getName(), doc1.getContentId());
+					GetResponse res = client.get(req, RequestOptions.DEFAULT);
+					assertThat(res.isExists(), is(true));
+
+					req = new GetRequest("spring-content-fulltext-index", doc1.getClass().getName(), doc2.getContentId());
+					res = client.get(req, RequestOptions.DEFAULT);
+					assertThat(res.isExists(), is(true));
 				});
 
-				Context("when a document is indexed", () -> {
+				Context("when the content is searched", () -> {
+
+					It("should become searchable", () -> {
+						assertThat(() -> store.search("one"), eventuallyEval(hasItem(doc1.getContentId()), Duration.ofSeconds(10)));
+
+						assertThat(() -> store.search("two"), eventuallyEval(hasItem(doc2.getContentId()), Duration.ofSeconds(10)));
+
+						assertThat(() -> store.search("one two"), eventuallyEval(hasItems(doc1.getContentId(), doc2.getContentId()), Duration.ofSeconds(10)));
+
+						assertThat(() -> store.search("+document +one -two"), eventuallyEval(hasItem(doc1.getContentId()), Duration.ofSeconds(10)));
+						assertThat(() -> store.search("+document +one -two"), eventuallyEval(hasItem(not(doc2.getContentId())), Duration.ofSeconds(10)));
+					});
+				});
+
+				Context("given that document is deleted", () -> {
 
 					BeforeEach(() -> {
-						id = UUID.randomUUID().toString();
+						id1 = doc1.getContentId();
+						store.unsetContent(doc1);
+						repo.delete(doc1);
 
-						IndexRequest req = new IndexRequest("test_index", "test_type", id);
-						req.setPipeline(attachment);
-
-						String source = "{" +
-								"\"data\": \"UWJveCBlbmFibGVzIGxhdW5jaGluZyBzdXBwb3J0ZWQsIGZ1bGx5LW1hbmFnZWQsIFJFU1RmdWwgRWxhc3RpY3NlYXJjaCBTZXJ2aWNlIGluc3RhbnRseS4g\"" +
-								"}";
-
-						req.source(source, XContentType.JSON);
-						IndexResponse res = client.index(req, RequestOptions.DEFAULT);
-						assertThat(res.getResult(), is(DocWriteResponse.Result.CREATED));
+						id2 = doc2.getContentId();
+						store.unsetContent(doc2);
+						repo.delete(doc2);
 					});
 
-					AfterEach(() -> {
-						DeleteRequest req = new DeleteRequest("test_index", "test_type", id);
-						DeleteResponse res = client.delete(req, RequestOptions.DEFAULT);
-						assertThat(res.getResult(), is(DocWriteResponse.Result.DELETED));
-					});
+					It("should delete the record of the content from the index", () -> {
+						GetRequest req = new GetRequest("spring-content-fulltext-index", doc1.getClass().getName(), id1);
+						GetResponse res = client.get(req, RequestOptions.DEFAULT);
+						assertThat(res.isExists(), is(false));
 
-					It("should be searchable", () -> {
-
-						GetRequest req = new GetRequest("test_index", "test_type", id);
-						GetResponse res2 = client.get(req, RequestOptions.DEFAULT);
-						assertThat(res2.isExists(), is(true));
-
-						assertThat(eval(() -> {
-							SearchRequest searchRequest = new SearchRequest("test_index");
-							searchRequest.types("test_type");
-
-							SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-							sourceBuilder.query(QueryBuilders.queryStringQuery("Service"));
-
-							SearchResponse res = client.search(searchRequest, RequestOptions.DEFAULT);
-
-							for (SearchHit hit : res.getHits().getHits()) {
-								if (hit.getId().equals(id)) {
-									return true;
-								}
-							}
-							return false;
-						}), eventuallyEval(is(true)));
-					});
-
-					Context("when other documents exist of a different type", () -> {
-
-						BeforeEach(() -> {
-							id2 = UUID.randomUUID().toString();
-
-							IndexRequest req = new IndexRequest("test_index_two", "test_type_two", id2);
-							req.setPipeline(attachment);
-
-							String source = "{" +
-									"\"data\": \"UWJveCBlbmFibGVzIGxhdW5jaGluZyBzdXBwb3J0ZWQsIGZ1bGx5LW1hbmFnZWQsIFJFU1RmdWwgRWxhc3RpY3NlYXJjaCBTZXJ2aWNlIGluc3RhbnRseS4g\"" +
-									"}";
-
-							req.source(source, XContentType.JSON);
-							IndexResponse res = client.index(req, RequestOptions.DEFAULT);
-							assertThat(res.getResult(), is(DocWriteResponse.Result.CREATED));
-						});
-
-						AfterEach(() -> {
-							DeleteRequest req = new DeleteRequest("test_index_two", "test_type_two", id2);
-							DeleteResponse res = client.delete(req, RequestOptions.DEFAULT);
-							assertThat(res.getResult(), is(DocWriteResponse.Result.DELETED));
-						});
-
-						It("should be still be searchable", () -> {
-
-							assertThat(eval(() -> {
-								SearchRequest searchRequest = new SearchRequest("test_index");
-								searchRequest.types("test_type");
-
-								SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-								sourceBuilder.query(QueryBuilders.queryStringQuery("Service"));
-
-								SearchResponse res = client.search(searchRequest, RequestOptions.DEFAULT);
-
-								for (SearchHit hit : res.getHits().getHits()) {
-									if (hit.getId().equals(id)) {
-										return true;
-									}
-								}
-
-								return false;
-							}), eventuallyEval(is(true)));
-
-							assertThat(eval(() -> {
-								SearchRequest searchRequest = new SearchRequest("test_index");
-								searchRequest.types("test_type");
-
-								SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-								sourceBuilder.query(QueryBuilders.queryStringQuery("Service"));
-
-								SearchResponse res = client.search(searchRequest, RequestOptions.DEFAULT);
-
-								for (SearchHit hit : res.getHits().getHits()) {
-									if (hit.getId().equals(id2)) {
-										return true;
-									}
-								}
-
-								return false;
-							}), eventuallyEval(is(false)));
-						});
+						req = new GetRequest("spring-content-fulltext-index", doc1.getClass().getName(), id2);
+						res = client.get(req, RequestOptions.DEFAULT);
+						assertThat(res.isExists(), is(false));
 					});
 				});
 			});
 		});
 	}
 
-	@FunctionalInterface
-	public interface ThrowingSupplier {
-		boolean accept() throws IOException;
+	@Test
+	public void noop() {
 	}
 
-	public static Supplier<Boolean> eval(ThrowingSupplier t) {
-		return () -> {
-			try {
-				return t.accept();
-			} catch (IOException ex) {
-				throw new AssertionError(ex);
-			}
-		};
+	public interface DocumentRepository extends CrudRepository<Document, Long> {
+		//
+	}
+
+	public interface DocumentContentStore extends ContentStore<Document, String>, Searchable<String> {
+		//
+	}
+
+	@Entity
+	@NoArgsConstructor
+	@Getter
+	@Setter
+	public static class Document {
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private Long id;
+
+		@ContentId
+		private String contentId;
+
+		private String title;
+		private String author;
 	}
 }
