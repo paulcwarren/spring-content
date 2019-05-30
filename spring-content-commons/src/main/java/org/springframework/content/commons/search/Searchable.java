79c101eeeacee5c3d2c4413c78f8b62d7c85da06
Paul Warren <paul.warren@emc.com>
2019-05-30 08:54:28 -0700

    Refactor Serchable interface
    - add new search accepting a query string; implementations should invoke the underlying search service query DSL
    - deprecate the find... methods

--- spring-content-commons/src/main/java/org/springframework/content/commons/search/Searchable.java
+++ spring-content-commons/src/main/java/org/springframework/content/commons/search/Searchable.java
@@ -2,17 +2,26 @@ package org.springframework.content.commons.search;
 
 public interface Searchable<T> {
 
+	Iterable<T> search(String queryString);
+
+	@Deprecated
 	Iterable<T> findKeyword(String query);
 
+	@Deprecated
 	Iterable<T> findAllKeywords(String... terms);
 
+	@Deprecated
 	Iterable<T> findAnyKeywords(String... terms);
 
+	@Deprecated
 	Iterable<T> findKeywordsNear(int proximity, String... terms);
 
+	@Deprecated
 	Iterable<T> findKeywordStartsWith(String term);
 
+	@Deprecated
 	Iterable<T> findKeywordStartsWithAndEndsWith(String a, String b);
 
+	@Deprecated
 	Iterable<T> findAllKeywordsWithWeights(String[] terms, double[] weights);
 }
