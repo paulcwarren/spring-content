d2f4fcd6a02388b37b2c108092c36b177665a8b1
Paul Warren <paul.warren@emc.com>
2019-05-30 20:52:21 -0700

    Remove unrequired Searchable

--- spring-content-rest/src/test/java/internal/org/springframework/content/rest/support/TestEntityContentRepository.java
+++ spring-content-rest/src/test/java/internal/org/springframework/content/rest/support/TestEntityContentRepository.java
@@ -1,14 +1,13 @@
 package internal.org.springframework.content.rest.support;
 
+import internal.org.springframework.content.rest.annotations.ContentStoreRestResource;
+
 import org.springframework.content.commons.renditions.Renderable;
 import org.springframework.content.commons.repository.ContentStore;
-import org.springframework.content.commons.search.Searchable;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
-import internal.org.springframework.content.rest.annotations.ContentStoreRestResource;
 
 @CrossOrigin(origins = "http://www.someurl.com")
 @ContentStoreRestResource(path = "testEntitiesContent")
 public interface TestEntityContentRepository
-		extends ContentStore<TestEntity, Long>, Renderable<TestEntity>, Searchable<Long> {
+		extends ContentStore<TestEntity, Long>, Renderable<TestEntity> {
 }
