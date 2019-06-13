a52e3aff6b6d86a3ea6a798a3fa9382a17555d9b
Paul Warren <paul.warren@emc.com>
2019-06-12 22:23:19 -0700

    Refactor the content links processor onto hateoas 1.0 api

--- spring-content-rest/src/main/java/org/springframework/content/rest/config/HypermediaConfiguration.java
+++ spring-content-rest/src/main/java/org/springframework/content/rest/config/HypermediaConfiguration.java
@@ -1,6 +1,7 @@
 package org.springframework.content.rest.config;
 
 import internal.org.springframework.content.rest.links.ContentLinksResourceProcessor;
+
 import org.springframework.content.commons.storeservice.ContentStoreService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,14 +9,14 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.repository.support.Repositories;
 import org.springframework.data.rest.core.mapping.RepositoryResourceMappings;
 import org.springframework.data.rest.webmvc.PersistentEntityResource;
-import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
 
 @Configuration
 @Import(RestConfiguration.class)
 public class HypermediaConfiguration {
 
 	@Bean
-	public ResourceProcessor<PersistentEntityResource> contentLinksProcessor(Repositories repos, ContentStoreService stores, RestConfiguration config, RepositoryResourceMappings mappings) {
+	public RepresentationModelProcessor<PersistentEntityResource> contentLinksProcessor(Repositories repos, ContentStoreService stores, RestConfiguration config, RepositoryResourceMappings mappings) {
 		return new ContentLinksResourceProcessor(repos, stores, config, mappings);
 	}
 }
