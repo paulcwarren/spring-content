dfa52c051375b1b351bb19dd959c1e0792dd0a78
Paul Warren <paul.warren@emc.com>
2019-07-02 10:07:12 -0700

    Add a spring boot level content rest configurer to ensure the base path property is passed on appropriately
    Fixes #59

--- spring-content-autoconfigure/src/main/java/internal/org/springframework/content/rest/boot/autoconfigure/ContentRestAutoConfiguration.java
+++ spring-content-autoconfigure/src/main/java/internal/org/springframework/content/rest/boot/autoconfigure/ContentRestAutoConfiguration.java
@@ -1,14 +1,15 @@
 package internal.org.springframework.content.rest.boot.autoconfigure;
 
+import java.net.URI;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.content.rest.config.RestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
-
-import java.net.URI;
 
 @Configuration
 @ConditionalOnWebApplication()
@@ -30,4 +31,10 @@ public class ContentRestAutoConfiguration {
 			this.baseUri = baseUri;
 		}
 	}
+
+	@Bean
+	public SpringBootContentRestConfigurer springBootContentRestConfigurer() {
+		return new SpringBootContentRestConfigurer();
+	}
+
 }
