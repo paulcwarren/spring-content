f3cc7e2a36a4cf1ce594e1441bd5753ded668a1a
Paul Warren <paul.warren@emc.com>
2019-07-11 20:50:15 -0700

    Add support to content links resource processor for multiple primitive content properties

---
+++ spring-content-rest/src/test/java/internal/org/springframework/content/rest/support/TestEntity2Store.java
@@ -0,0 +1,8 @@
+package internal.org.springframework.content.rest.support;
+
+import java.util.UUID;
+
+import org.springframework.content.commons.repository.ContentStore;
+
+public interface TestEntity2Store extends ContentStore<TestEntity2, UUID> {
+}
---
+++ spring-content-rest/src/test/java/internal/org/springframework/content/rest/support/TestEntity5Store.java
@@ -0,0 +1,6 @@
+package internal.org.springframework.content.rest.support;
+
+import org.springframework.content.commons.repository.ContentStore;
+
+public interface TestEntity5Store extends ContentStore<TestEntity5, Long> {
+}
