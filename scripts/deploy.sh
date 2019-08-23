f9d90fc644441cef360c689c70688fe281406b49
Paul Warren <paul.warren@emc.com>
2019-08-22 23:00:12 -0700

    Refactor the travis build
    - turn on jacocoa and coveralls reporting
    - turn off slack notifications
    - remove the redundant 'install' mvn build step
    - make docs publish to snapshots/ or releases/

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,7 @@
 if [ ! -z "$TRAVIS_TAG" ]
 then
-    echo "On a tag -> set pom.xml <version> to $TRAVIS_TAG and BUILD_TYPE to release"
-    export BUILD_TYPE=release
+    export BUILD_TYPE=release/$TRAVIS_TAG
     mvn org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG
-    #1>/dev/null 2>/dev/null
-else
-    echo "Not on a tag -> keep snapshot version in pom.xml"
 fi
 
 echo BUILD_TYPE: $BUILD_TYPE
@@ -14,4 +10,4 @@ echo "Importing GPG keys"
 openssl aes-256-cbc -K $encrypted_2d46c2ddc73e_key -iv $encrypted_2d46c2ddc73e_iv -in codesigning.asc.enc -out codesigning.asc -d
 gpg --fast-import codesigning.asc
 
-mvn -B -U -P ci,docs deploy --settings settings.xml -DskipTests=true scm-publish:publish-scm
+mvn -B -U -P ci,docs deploy scm-publish:publish-scm -DskipTests=true --settings settings.xml
