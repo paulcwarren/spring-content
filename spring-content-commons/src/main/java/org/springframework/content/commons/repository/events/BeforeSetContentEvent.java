b952a83cbf3e7c524e7bb7dcbd554d797e076909
Paul Warren <paul.warren@emc.com>
2020-02-20 22:47:36 -0800

    Spike on using a teeinputstream to deliver content to beforesetcontent event

--- spring-content-commons/src/main/java/org/springframework/content/commons/repository/events/BeforeSetContentEvent.java
+++ spring-content-commons/src/main/java/org/springframework/content/commons/repository/events/BeforeSetContentEvent.java
@@ -1,17 +1,22 @@
 package org.springframework.content.commons.repository.events;
 
+import java.io.InputStream;
 import java.io.Serializable;
+
+import lombok.Getter;
 
 import org.springframework.content.commons.repository.StoreEvent;
 import org.springframework.content.commons.repository.ContentStore;
 
+@Getter
 public class BeforeSetContentEvent extends StoreEvent {
 
 	private static final long serialVersionUID = -7299354365313770L;
 
-	public BeforeSetContentEvent(Object source,
-			ContentStore<Object, Serializable> store) {
-		super(source, store);
-	}
+	private final InputStream is;
 
+	public BeforeSetContentEvent(Object source, ContentStore<Object, Serializable> store, InputStream is) {
+		super(source, store);
+		this.is = is;
+	}
 }
---
+++ spring-content-fs/src/test/java/it/events/BeforeSetEventIT.java
@@ -0,0 +1,99 @@
+package it.events;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jSpringRunner;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.content.commons.annotations.ContentId;
+import org.springframework.content.commons.annotations.HandleBeforeSetContent;
+import org.springframework.content.commons.annotations.StoreEventHandler;
+import org.springframework.content.commons.repository.ContentStore;
+import org.springframework.content.commons.repository.events.BeforeSetContentEvent;
+import org.springframework.content.fs.config.EnableFilesystemStores;
+import org.springframework.content.fs.io.FileSystemResourceLoader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Context;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Describe;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.It;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+@RunWith(Ginkgo4jSpringRunner.class)
+@ContextConfiguration(classes = { BeforeSetEventIT.TestConfig.class })
+public class BeforeSetEventIT {
+
+    @Autowired
+    private TestEntityContentStore store;
+
+    {
+        Describe("BeforeSetEvent InputStream Access", () -> {
+
+            Context("when the content input stream is consumed by the beforesetevent", () -> {
+
+                It("should still set the content in the store", () -> {
+                    TestEntity te = new TestEntity();
+                    ByteArrayInputStream bais = new ByteArrayInputStream("BeforeSetEvent content".getBytes());
+                    te = store.setContent(te, bais);
+                    IOUtils.closeQuietly(bais);
+                    try (InputStream foo = store.getContent(te)) {
+                        assertThat(IOUtils.toString(foo), is("BeforeSetEvent content"));
+                    }
+                });
+            });
+        });
+    }
+
+    @Configuration
+    @EnableFilesystemStores
+    public static class TestConfig {
+
+        @Bean
+        FileSystemResourceLoader fs() throws IOException {
+            return new FileSystemResourceLoader(Files.createTempDirectory("").toFile().getAbsolutePath());
+        }
+
+        @Bean
+        public ExampleAnnotatedEventHandler eventHandler() {
+            return new ExampleAnnotatedEventHandler();
+        }
+
+        @StoreEventHandler
+        public static class ExampleAnnotatedEventHandler {
+
+            @HandleBeforeSetContent
+            public void handleBeforeSetContentEvent(BeforeSetContentEvent event) throws IOException {
+                InputStream is = event.getIs();
+                IOUtils.copy(is, new ByteArrayOutputStream());
+            }
+        }
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public class TestEntity {
+        @ContentId
+        private String contentId;
+    }
+
+    public interface TestEntityContentStore extends ContentStore<TestEntity, String> {
+    }
+
+    @Test
+    public void noop() {}
+}
