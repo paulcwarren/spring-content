abfccdd6dcc674745e553fd5c5a06e49eee761cc
Paul Warren <paul_c_warren@yahoo.com>
2020-05-27 21:46:45 -0700

    Add documentation for the new s3 multi-tenant configuration feature

--- spring-content-s3/src/main/java/org/springframework/content/s3/config/MultiTenantAmazonS3Provider.java
+++ spring-content-s3/src/main/java/org/springframework/content/s3/config/MultiTenantAmazonS3Provider.java
@@ -3,8 +3,8 @@ package org.springframework.content.s3.config;
 import com.amazonaws.services.s3.AmazonS3;
 
 /**
- * A callback, used by the S3 default store implementation when returning a resource, in order to establish which
- * AmazonS3 client to provide to the Resource being returned.
+ * When configured to do so, the S3Store will provide the AmazonS3 client object returned by this function to any
+ * `Resource`s that it is asked to load.
  */
 public interface MultiTenantAmazonS3Provider {
 
