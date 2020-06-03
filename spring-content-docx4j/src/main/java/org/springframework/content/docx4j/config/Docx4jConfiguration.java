fb415135bf4852bd54207b962dec589dda584d57
Paul Warren <paul_c_warren@yahoo.com>
2020-06-02 22:27:33 -0700

    Move JpegToPngRenderer from docx4j to renditions
    #226

--- spring-content-docx4j/src/main/java/org/springframework/content/docx4j/config/Docx4jConfiguration.java
+++ spring-content-docx4j/src/main/java/org/springframework/content/docx4j/config/Docx4jConfiguration.java
@@ -1,10 +1,10 @@
 package org.springframework.content.docx4j.config;
 
-import internal.org.springframework.content.docx4j.JpegToPngRenditionProvider;
+import internal.org.springframework.content.docx4j.WordToPdfRenditionProvider;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = JpegToPngRenditionProvider.class)
+@ComponentScan(basePackageClasses = WordToPdfRenditionProvider.class)
 public class Docx4jConfiguration {
 }
---
+++ spring-content-renditions/src/main/java/org/springframework/content/renditions/config/RenditionsConfiguration.java
@@ -0,0 +1,10 @@
+package org.springframework.content.renditions.config;
+
+import org.springframework.content.renditions.renderers.PdfToJpegRenderer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses= PdfToJpegRenderer.class)
+public class RenditionsConfiguration {
+}
