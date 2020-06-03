4c470ad22df147ff552bbce86e2fd12a63671fa7
Paul Warren <paul_c_warren@yahoo.com>
2020-06-02 21:06:09 -0700

    When spring-content-docx4j is on the classpath import its renderers
    #226

--- spring-content-autoconfigure/src/main/java/internal/org/springframework/content/renditions/boot/autoconfigure/RenditionsContentAutoConfiguration.java
+++ spring-content-autoconfigure/src/main/java/internal/org/springframework/content/renditions/boot/autoconfigure/RenditionsContentAutoConfiguration.java
@@ -1,10 +1,20 @@
 package internal.org.springframework.content.renditions.boot.autoconfigure;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.content.docx4j.config.Docx4jConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan("org.springframework.content.renditions.renderers")
 public class RenditionsContentAutoConfiguration {
+
+    @Configuration
+    @ConditionalOnClass(Docx4jConfiguration.class)
+    @Import(Docx4jConfiguration.class)
+    public static class Docx4jAutoConfiguration {
+        //
+    }
 
 }
