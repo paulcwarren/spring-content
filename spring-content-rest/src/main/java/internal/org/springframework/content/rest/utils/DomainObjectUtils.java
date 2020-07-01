82812f6472510a2298f913973b601cfcb42ec97d
Paul Warren <paul_c_warren@yahoo.com>
2020-06-30 23:03:02 -0700

    Add class check before attempting to find javax.persistence.Id annotation

--- spring-content-rest/src/main/java/internal/org/springframework/content/rest/utils/DomainObjectUtils.java
+++ spring-content-rest/src/main/java/internal/org/springframework/content/rest/utils/DomainObjectUtils.java
@@ -4,11 +4,19 @@ import org.springframework.content.commons.utils.BeanUtils;
 
 public final class DomainObjectUtils {
 
+    private static boolean JAVAX_PERSISTENCE_ID_CLASS_PRESENT = false;
+
+    static {
+        try {
+            JAVAX_PERSISTENCE_ID_CLASS_PRESENT = DomainObjectUtils.class.getClassLoader().loadClass("javax.persistence.Id") != null;
+        } catch (ClassNotFoundException e) {}
+    }
+
     private DomainObjectUtils() {}
 
     public static final Object getId(Object entity) {
 
-        if (BeanUtils.hasFieldWithAnnotation(entity, javax.persistence.Id.class)) {
+        if (JAVAX_PERSISTENCE_ID_CLASS_PRESENT && BeanUtils.hasFieldWithAnnotation(entity, javax.persistence.Id.class)) {
             return BeanUtils.getFieldWithAnnotation(entity, javax.persistence.Id.class);
         } else if (BeanUtils.hasFieldWithAnnotation(entity, org.springframework.data.annotation.Id.class)) {
             return BeanUtils.getFieldWithAnnotation(entity, org.springframework.data.annotation.Id.class);
