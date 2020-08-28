a43f8f1ae29548f7e39bb92ab2d13ab89c77d183
Paul Warren <paul_c_warren@yahoo.com>
2020-08-27 21:23:51 -0700

    DRY up SearchableImpl

--- spring-content-solr/src/main/java/internal/org/springframework/content/fragments/SearchableImpl.java
+++ spring-content-solr/src/main/java/internal/org/springframework/content/fragments/SearchableImpl.java
@@ -11,7 +11,6 @@ import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.request.QueryRequest;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.util.NamedList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.content.commons.annotations.ContentId;
 import org.springframework.content.commons.fragments.ContentStoreAware;
@@ -52,12 +51,12 @@ public class SearchableImpl implements Searchable<Object>, ContentStoreAware {
 
     @Override
     public List<Object> search(String queryStr) {
-        return getIds(executeQuery(this.getDomainClass(), queryStr, null));
+        return getResults(executeQuery(this.getDomainClass(), queryStr, null, String.class), String.class);
     }
 
     @Override
     public List<Object> search(String queryStr, Pageable pageable) {
-        return getIds(executeQuery(this.getDomainClass(), queryStr, pageable));
+        return getResults(executeQuery(this.getDomainClass(), queryStr, pageable, String.class), String.class);
     }
 
     @Override
@@ -67,44 +66,44 @@ public class SearchableImpl implements Searchable<Object>, ContentStoreAware {
 
     @Override
     public List<Object> findKeyword(String queryStr) {
-        return getIds(executeQuery(this.getDomainClass(), queryStr, null));
+        return getResults(executeQuery(this.getDomainClass(), queryStr, null, String.class), String.class);
     }
 
     @Override
     public List<Object> findAllKeywords(String... terms) {
         String queryStr = this.parseTerms("AND", terms);
-        return getIds(executeQuery(this.getDomainClass(), queryStr, null));
+        return getResults(executeQuery(this.getDomainClass(), queryStr, null, String.class), String.class);
     }
 
     @Override
     public List<Object> findAnyKeywords(String... terms) {
         String queryStr = this.parseTerms("OR", terms);
-        return getIds(executeQuery(this.getDomainClass(), queryStr, null));
+        return getResults(executeQuery(this.getDomainClass(), queryStr, null, String.class), String.class);
     }
 
     @Override
     public List<Object> findKeywordsNear(int proximity, String... terms) {
         String termStr = this.parseTerms("NONE", terms);
         String queryStr = "\"" + termStr + "\"~" + Integer.toString(proximity);
-        return getIds(executeQuery(this.getDomainClass(), queryStr, null));
+        return getResults(executeQuery(this.getDomainClass(), queryStr, null, String.class), String.class);
     }
 
     @Override
     public List<Object> findKeywordStartsWith(String term) {
         String queryStr = term + "*";
-        return getIds(executeQuery(this.getDomainClass(), queryStr, null));
+        return getResults(executeQuery(this.getDomainClass(), queryStr, null, String.class), String.class);
     }
 
     @Override
     public List<Object> findKeywordStartsWithAndEndsWith(String a, String b) {
         String queryStr = a + "*" + b;
-        return getIds(executeQuery(this.getDomainClass(), queryStr, null));
+        return getResults(executeQuery(this.getDomainClass(), queryStr, null, String.class), String.class);
     }
 
     @Override
     public List<Object> findAllKeywordsWithWeights(String[] terms, double[] weights) {
         String queryStr = parseTermsAndWeights("AND", terms, weights);
-        return getIds(executeQuery(this.getDomainClass(), queryStr, null));
+        return getResults(executeQuery(this.getDomainClass(), queryStr, null, String.class), String.class);
     }
 
     /* package */ String parseTermsAndWeights(String operator, String[] terms,
@@ -146,65 +145,10 @@ public class SearchableImpl implements Searchable<Object>, ContentStoreAware {
         return builder.toString();
     }
 
-    /* package */ List<Object> getIds(NamedList response) {
-        List<Object> ids = new ArrayList<>();
-        SolrDocumentList list = (SolrDocumentList) response.get("response");
-        for (int j = 0; j < list.size(); ++j) {
-            String id = list.get(j).getFieldValue("id").toString();
-            id = id.substring(id.indexOf(':') + 1, id.length());
-            ids.add(id);
-        }
-
-        return ids;
-    }
-
     /* package */ QueryRequest solrAuthenticate(QueryRequest request) {
         request.setBasicAuthCredentials(solrProperties.getUser(),
                 solrProperties.getPassword());
         return request;
-    }
-
-    /* package */ NamedList<Object> executeQuery(Class<?> domainClass, String queryString, Pageable pageable) {
-        SolrQuery query = new SolrQuery();
-        query.setQuery("_text_:" + queryString);
-
-        query.addFilterQuery("id:" + domainClass.getCanonicalName() + "\\:*");
-
-        if (this.filterProvider != null) {
-            query.addFilterQuery(this.filterProvider.filterQueries(domainClass));
-        }
-
-        query.setFields(field);
-
-        query.setHighlight(true);
-
-        if (pageable != null) {
-            query.setStart(pageable.getPageNumber() * pageable.getPageSize());
-            query.setRows(pageable.getPageSize());
-        }
-
-        QueryRequest request = new QueryRequest(query);
-        if (solrProperties.getUser() != null) {
-            request = solrAuthenticate(request);
-        }
-
-        NamedList<Object> response = null;
-        try {
-            response = solr.request(request, null);
-        }
-        catch (SolrServerException e) {
-            throw new StoreAccessException(
-                    String.format("Error running query %s on field %s against solr.",
-                            queryString, field),
-                    e);
-        }
-        catch (IOException e) {
-            throw new StoreAccessException(
-                    String.format("Error running query %s on field %s against solr.",
-                            queryString, field),
-                    e);
-        }
-        return response;
     }
 
     /* package */ QueryResponse executeQuery(Class<?> domainClass, String queryString, Pageable pageable, Class<?> resultType) {
@@ -252,27 +196,30 @@ public class SearchableImpl implements Searchable<Object>, ContentStoreAware {
         SolrDocumentList list = response.getResults();
         for (int j = 0; j < list.size(); ++j) {
 
-            Object result = null;
-            try {
-                result = searchType.newInstance();
-            } catch (InstantiationException | IllegalAccessException e) {
-                e.printStackTrace();
-            }
-
             String id = list.get(j).getFieldValue("id").toString();
-            String strippedId = id.substring(id.indexOf(':')
-                    + 1, id.length());
-            if (BeanUtils.findFieldWithAnnotation(searchType, ContentId.class) != null) {
-                BeanUtils.setFieldWithAnnotation(result, ContentId.class, strippedId);
-            }
+            String strippedId = id.substring(id.indexOf(':') + 1, id.length());
 
-            if (BeanUtils.findFieldWithAnnotation(searchType, Highlight.class) != null) {
-                Map<String, Map<String, List<String>>> highlights = response.getHighlighting();
-                List<String> highlight = highlights.get(id).get("_text_");
-                BeanUtils.setFieldWithAnnotation(result, Highlight.class, highlight.get(0));
-            }
+            if (searchType.isPrimitive() || String.class.equals(searchType)) {
+                results.add(strippedId);
+            } else {
+                Object result = null;
+                try {
+                    result = searchType.newInstance();
+                } catch (InstantiationException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
 
-            results.add(result);
+                if (BeanUtils.findFieldWithAnnotation(searchType, ContentId.class) != null) {
+                    BeanUtils.setFieldWithAnnotation(result, ContentId.class, strippedId);
+                }
+
+                if (BeanUtils.findFieldWithAnnotation(searchType, Highlight.class) != null) {
+                    Map<String, Map<String, List<String>>> highlights = response.getHighlighting();
+                    List<String> highlight = highlights.get(id).get("_text_");
+                    BeanUtils.setFieldWithAnnotation(result, Highlight.class, highlight.get(0));
+                }
+                results.add(result);
+            }
         }
 
         return results;
