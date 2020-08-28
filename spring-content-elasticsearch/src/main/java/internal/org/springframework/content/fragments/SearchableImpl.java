deb08193bb978213dcd090232107e9e93c9f75f2
Paul Warren <paul_c_warren@yahoo.com>
2020-08-27 23:03:21 -0700

    Mark new search api unsupported

--- spring-content-elasticsearch/src/main/java/internal/org/springframework/content/fragments/SearchableImpl.java
+++ spring-content-elasticsearch/src/main/java/internal/org/springframework/content/fragments/SearchableImpl.java
@@ -1,12 +1,12 @@
 package internal.org.springframework.content.fragments;
 
+import static java.lang.String.format;
+
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import internal.org.springframework.content.elasticsearch.ElasticsearchIndexer;
-import internal.org.springframework.content.elasticsearch.IndexManager;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.elasticsearch.ElasticsearchStatusException;
@@ -18,144 +18,149 @@ import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.content.commons.repository.StoreAccessException;
 import org.springframework.content.commons.search.Searchable;
 import org.springframework.data.domain.Pageable;
 
-import static java.lang.String.format;
+import internal.org.springframework.content.elasticsearch.ElasticsearchIndexer;
+import internal.org.springframework.content.elasticsearch.IndexManager;
 
 public class SearchableImpl implements Searchable<Serializable> {
 
     private static final Log LOGGER = LogFactory.getLog(ElasticsearchIndexer.class);
 
-	private final RestHighLevelClient client;
-	private final IndexManager manager;
+    private final RestHighLevelClient client;
+    private final IndexManager manager;
 
-	private Class<?> domainClass;
+    private Class<?> domainClass;
 
-	public SearchableImpl() {
-		client = null;
-		manager = null;
-		domainClass = null;
-	}
+    public SearchableImpl() {
+        client = null;
+        manager = null;
+        domainClass = null;
+    }
 
-	@Autowired
-	public SearchableImpl(RestHighLevelClient client, IndexManager manager) {
-		this.client = client;
-		this.manager = manager;
-	}
+    @Autowired
+    public SearchableImpl(RestHighLevelClient client, IndexManager manager) {
+        this.client = client;
+        this.manager = manager;
+    }
 
-	public void setDomainClass(Class<?> domainClass) {
-		this.domainClass = domainClass;
-	}
+    public void setDomainClass(Class<?> domainClass) {
+        this.domainClass = domainClass;
+    }
 
-	@Override
-	public Iterable<Serializable> search(String queryStr) {
-		SearchRequest searchRequest = new SearchRequest(manager.indexName(domainClass));
-		searchRequest.types(domainClass.getName());
+    @Override
+    public Iterable<Serializable> search(String queryStr) {
+        SearchRequest searchRequest = new SearchRequest(manager.indexName(domainClass));
+        searchRequest.types(domainClass.getName());
 
-		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-		sourceBuilder.query(QueryBuilders.simpleQueryStringQuery(queryStr));
-		searchRequest.source(sourceBuilder);
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.simpleQueryStringQuery(queryStr));
+        searchRequest.source(sourceBuilder);
 
-		SearchResponse res = null;
-		try {
-			res = client.search(searchRequest, RequestOptions.DEFAULT);
-		}
-		catch (IOException | ElasticsearchStatusException e) {
-		    LOGGER.error(format("Error searching indexed content for '%s'", queryStr), e);
-			throw new StoreAccessException(format("Error searching indexed content for '%s'", queryStr), e);
-		}
+        SearchResponse res = null;
+        try {
+            res = client.search(searchRequest, RequestOptions.DEFAULT);
+        }
+        catch (IOException | ElasticsearchStatusException e) {
+            LOGGER.error(format("Error searching indexed content for '%s'", queryStr), e);
+            throw new StoreAccessException(format("Error searching indexed content for '%s'", queryStr), e);
+        }
 
-		return getIDs(res.getHits());
-	}
+        return getIDs(res.getHits());
+    }
 
-	@Override
-	public List<Serializable> search(String queryStr, Pageable pageable) {
-		SearchRequest searchRequest = new SearchRequest(manager.indexName(domainClass));
-		searchRequest.types(domainClass.getName());
+    @Override
+    public List<Serializable> search(String queryStr, Pageable pageable) {
+        SearchRequest searchRequest = new SearchRequest(manager.indexName(domainClass));
+        searchRequest.types(domainClass.getName());
 
-		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-		sourceBuilder.query(QueryBuilders.simpleQueryStringQuery(queryStr));
-		sourceBuilder.from(pageable.getPageNumber() * pageable.getPageSize());
-		sourceBuilder.size(pageable.getPageSize());
-		searchRequest.source(sourceBuilder);
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.simpleQueryStringQuery(queryStr));
+        sourceBuilder.from(pageable.getPageNumber() * pageable.getPageSize());
+        sourceBuilder.size(pageable.getPageSize());
+        searchRequest.source(sourceBuilder);
 
-		SearchResponse res = null;
-		try {
-			res = client.search(searchRequest, RequestOptions.DEFAULT);
-		}
-		catch (IOException | ElasticsearchStatusException e) {
-			LOGGER.error(format("Error searching indexed content for '%s'", queryStr), e);
-			throw new StoreAccessException(format("Error searching indexed content for '%s'", queryStr), e);
-		}
+        SearchResponse res = null;
+        try {
+            res = client.search(searchRequest, RequestOptions.DEFAULT);
+        }
+        catch (IOException | ElasticsearchStatusException e) {
+            LOGGER.error(format("Error searching indexed content for '%s'", queryStr), e);
+            throw new StoreAccessException(format("Error searching indexed content for '%s'", queryStr), e);
+        }
 
-		return getIDs(res.getHits());
-	}
+        return getIDs(res.getHits());
+    }
 
-	@Override
-	public Iterable<Serializable> findKeyword(String query) {
-		SearchRequest searchRequest = new SearchRequest(manager.indexName(domainClass));
-		searchRequest.types(domainClass.getName());
+    @Override
+    public List<Serializable> search(String queryString, Pageable pageable, Class<? extends Serializable> searchType) {
+        throw new UnsupportedOperationException();
+    }
 
-		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-		sourceBuilder.query(QueryBuilders.queryStringQuery(query));
-		searchRequest.source(sourceBuilder);
+    @Override
+    public Iterable<Serializable> findKeyword(String query) {
+        SearchRequest searchRequest = new SearchRequest(manager.indexName(domainClass));
+        searchRequest.types(domainClass.getName());
 
-		SearchResponse res = null;
-		try {
-			res = client.search(searchRequest, RequestOptions.DEFAULT);
-		}
-		catch (IOException ioe) {
-			throw new StoreAccessException(format("Error searching indexed content for '%s'", query), ioe);
-		}
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.queryStringQuery(query));
+        searchRequest.source(sourceBuilder);
 
-		return getIDs(res.getHits());
-	}
+        SearchResponse res = null;
+        try {
+            res = client.search(searchRequest, RequestOptions.DEFAULT);
+        }
+        catch (IOException ioe) {
+            throw new StoreAccessException(format("Error searching indexed content for '%s'", query), ioe);
+        }
 
-	@Override
-	public Iterable<Serializable> findAllKeywords(String... terms) {
-		throw new UnsupportedOperationException();
-	}
+        return getIDs(res.getHits());
+    }
 
-	@Override
-	public Iterable<Serializable> findAnyKeywords(String... terms) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public Iterable<Serializable> findAllKeywords(String... terms) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public Iterable<Serializable> findKeywordsNear(int proximity, String... terms) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public Iterable<Serializable> findAnyKeywords(String... terms) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public Iterable<Serializable> findKeywordStartsWith(String term) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public Iterable<Serializable> findKeywordsNear(int proximity, String... terms) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public Iterable<Serializable> findKeywordStartsWithAndEndsWith(String a, String b) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public Iterable<Serializable> findKeywordStartsWith(String term) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public Iterable<Serializable> findAllKeywordsWithWeights(String[] terms, double[] weights) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public Iterable<Serializable> findKeywordStartsWithAndEndsWith(String a, String b) {
+        throw new UnsupportedOperationException();
+    }
 
-	private List<Serializable> getIDs(SearchHits result) {
-		List<Serializable> contents = new ArrayList<>();
+    @Override
+    public Iterable<Serializable> findAllKeywordsWithWeights(String[] terms, double[] weights) {
+        throw new UnsupportedOperationException();
+    }
 
-		if (result == null || result.getTotalHits().value == 0) {
-			return contents;
-		}
+    private List<Serializable> getIDs(SearchHits result) {
+        List<Serializable> contents = new ArrayList<>();
 
-		for (SearchHit hit : result.getHits()) {
-			contents.add(hit.getId());
-		}
+        if (result == null || result.getTotalHits().value == 0) {
+            return contents;
+        }
 
-		return contents;
-	}
+        for (SearchHit hit : result.getHits()) {
+            contents.add(hit.getId());
+        }
+
+        return contents;
+    }
 }
