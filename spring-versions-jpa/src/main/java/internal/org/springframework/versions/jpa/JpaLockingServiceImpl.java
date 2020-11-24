8e8eeab5f36357bdfe9075cdab0164bab9c06eb5
Paul Warren <paul_c_warren@yahoo.com>
2020-11-23 23:23:07 -0800

    LockingService allows multiple calls to lock by the same principal

--- spring-versions-jpa/src/main/java/internal/org/springframework/versions/jpa/JpaLockingServiceImpl.java
+++ spring-versions-jpa/src/main/java/internal/org/springframework/versions/jpa/JpaLockingServiceImpl.java
@@ -1,11 +1,15 @@
 package internal.org.springframework.versions.jpa;
 
-import internal.org.springframework.versions.LockingService;
+import java.security.Principal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.security.auth.Subject;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DeadlockLoserDataAccessException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -13,14 +17,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Service;
 
-import javax.security.auth.Subject;
-import javax.sql.RowSet;
-import java.security.Principal;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
-import static java.lang.String.format;
+import internal.org.springframework.versions.LockingService;
 
 @Service
 public class JpaLockingServiceImpl implements LockingService {
@@ -40,9 +37,16 @@ public class JpaLockingServiceImpl implements LockingService {
             throw new SecurityException("no principal");
         }
 
+        String sql = "SELECT count(lock_owner) from locks where entity_id = ?  and lock_owner = ?";
+        Integer rc = template.queryForObject(sql, new String[] {entityId.toString(), principal.getName()}, Integer.class);
+
+        if (rc == 1) {
+            return true;
+        }
+
         try {
-            String sql = "INSERT INTO locks (entity_id, lock_owner) VALUES (?,?)";
-            int rc = template.update(sql, entityId.toString(), principal.getName());
+            sql = "INSERT INTO locks (entity_id, lock_owner) VALUES (?,?)";
+            rc = template.update(sql, entityId.toString(), principal.getName());
             return (rc == 1);
         } catch (DuplicateKeyException e) {
             return false;
@@ -65,6 +69,7 @@ public class JpaLockingServiceImpl implements LockingService {
         String sql = "SELECT lock_owner from locks where entity_id = '" + entityId + "'";
         List<String> lockOwners = template.query(sql, new RowMapper() {
 
+            @Override
             public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
                 return rs.getString(1);
             }
