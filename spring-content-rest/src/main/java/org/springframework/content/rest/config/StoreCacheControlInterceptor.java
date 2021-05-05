9418177be13c9f48ec259554e91d1a088207385e
Paul Warren <paul_c_warren@yahoo.com>
2021-05-04 22:38:10 -0700

    Support for configuring cache control headers
    Fixes #503

---
+++ spring-content-rest/src/main/java/org/springframework/content/rest/config/StoreCacheControlInterceptor.java
@@ -0,0 +1,102 @@
+package org.springframework.content.rest.config;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.CacheControl;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.util.UrlPathHelper;
+
+import internal.org.springframework.content.rest.utils.StoreUtils;
+import lombok.Getter;
+
+public class StoreCacheControlInterceptor extends HandlerInterceptorAdapter {
+
+    private Map<String, CacheControl> cacheControlMap = new HashMap<>();
+    private List<CacheControlRule> cacheControlRules = new ArrayList<>();
+    private URI baseUri;
+
+    public StoreCacheControlInterceptor() {
+
+    }
+
+    public StoreCacheControlConfigurer configurer() {
+        return new StoreCacheControlConfigurer(this);
+    }
+
+    public void addCacheControl(String mapping, CacheControl cacheControl) {
+        cacheControlMap.put(mapping, cacheControl);
+    }
+
+    public void addCacheControlRule(CacheControlRule cacheControlRule) {
+        cacheControlRules.add(cacheControlRule);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        if (!"GET".equals(request.getMethod())) {
+            return true;
+        }
+
+        UrlPathHelper pathHelper = UrlPathHelper.defaultInstance;
+
+        String lookupPath = pathHelper.getLookupPathForRequest(request);
+        String storeLookupPath = StoreUtils.storeLookupPath(lookupPath, baseUri);
+
+        CacheControl cacheControl = cacheControlMap.get(storeLookupPath);
+
+        for (CacheControlRule rule : cacheControlRules) {
+
+            if (rule.match(storeLookupPath)) {
+                response.addHeader("Cache-Control", rule.getCacheControl().getHeaderValue());
+            }
+        }
+
+        return true;
+    }
+
+    public void setBaseUri(URI baseUri) {
+        this.baseUri = baseUri;
+    }
+
+    @Getter
+    static class CacheControlRule {
+
+        private static AntPathMatcher matcher = new AntPathMatcher();
+
+        private String pattern;
+        private CacheControl cacheControl;
+
+        public CacheControlRule(String pattern, CacheControl control) {
+            this.pattern = pattern;
+            this.cacheControl = control;
+        }
+
+        public boolean match(String path) {
+            return matcher.match(pattern, path);
+        }
+    }
+
+    public static class StoreCacheControlConfigurer {
+
+        private final StoreCacheControlInterceptor interceptor;
+
+        public StoreCacheControlConfigurer(StoreCacheControlInterceptor interceptor) {
+            this.interceptor = interceptor;
+        }
+
+        public StoreCacheControlConfigurer antMatcher(String pattern, CacheControl cacheControl) {
+            interceptor.addCacheControlRule(new CacheControlRule(pattern, cacheControl));
+            return this;
+        }
+    }
+}
---
+++ spring-content-rest/src/test/java/internal/org/springframework/content/rest/it/cachecontrol/CacheControlIT.java
@@ -0,0 +1,214 @@
+package internal.org.springframework.content.rest.it.cachecontrol;
+
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.BeforeEach;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Describe;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.It;
+import static com.jayway.restassured.RestAssured.given;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.Duration;
+import java.util.UUID;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.content.fs.config.EnableFilesystemStores;
+import org.springframework.content.fs.io.FileSystemResourceLoader;
+import org.springframework.content.fs.store.FilesystemContentStore;
+import org.springframework.content.rest.config.ContentRestConfigurer;
+import org.springframework.content.rest.config.RestConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.CacheControl;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jConfiguration;
+import com.github.paulcwarren.ginkgo4j.Ginkgo4jSpringRunner;
+import com.jayway.restassured.RestAssured;
+
+import internal.org.springframework.content.rest.it.SecurityConfiguration;
+import internal.org.springframework.content.rest.support.TestEntity;
+
+@RunWith(Ginkgo4jSpringRunner.class)
+@Ginkgo4jConfiguration(threads=1)
+@SpringBootTest(classes = CacheControlIT.Application.class, webEnvironment=WebEnvironment.RANDOM_PORT)
+public class CacheControlIT {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Autowired
+    private CacheControlRepository repo;
+
+    @Autowired
+    private CacheControlStore store;
+
+    @LocalServerPort
+    int port;
+
+    {
+        Describe("CacheControl", () -> {
+
+            BeforeEach(() -> {
+                RestAssured.port = port;
+            });
+
+            It("given the pattern matches it should apply the configured cache settings", () -> {
+
+                TestEntity tentity = new TestEntity();
+                tentity = repo.save(tentity);
+                tentity = store.setContent(tentity, new ByteArrayInputStream("some content".getBytes()));
+                tentity.setMimeType("text/plain");
+                tentity = repo.save(tentity);
+
+                given()
+                        .accept("text/plain")
+                .when()
+                    .get("/testEntities/" + tentity.getId())
+                .then()
+                    .statusCode(200)
+                    .header("Cache-Control", "max-age=60");
+            });
+
+            It("given the pattern doesn't match it should not apply the configured cache settings", () -> {
+
+                TestEntity tentity = new TestEntity();
+                tentity = repo.save(tentity);
+                tentity = store.setContent(tentity, new ByteArrayInputStream("some content".getBytes()));
+                tentity.setMimeType("text/plain");
+                tentity = repo.save(tentity);
+
+                given()
+                        .accept("text/plain")
+                .when()
+                    .get("/testEntities/" + tentity.getId() + "/content")
+                .then()
+                    .statusCode(200)
+                    .header("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
+            });
+        });
+    }
+
+    public interface CacheControlRepository extends CrudRepository<TestEntity, Long> {
+    }
+
+    private interface CacheControlStore extends FilesystemContentStore<TestEntity, UUID> {
+    }
+
+    @SpringBootApplication
+    @ComponentScan(excludeFilters={
+          @Filter(type = FilterType.REGEX,
+                pattern = {
+                      ".*MongoConfiguration"
+          })
+    })
+    public static class Application {
+
+       public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
+
+       @Configuration
+       @Import({RestConfiguration.class, SecurityConfiguration.class})
+       @EnableJpaRepositories(basePackages="internal.org.springframework.content.rest.it.cachecontrol", considerNestedRepositories = true)
+       @EnableFilesystemStores(basePackages="internal.org.springframework.content.rest.it.cachecontrol")
+       public static class AppConfig {
+
+           @Value("/org/springframework/content/jpa/schema-drop-h2.sql")
+           private ClassPathResource dropReopsitoryTables;
+
+           @Value("/org/springframework/content/jpa/schema-h2.sql")
+           private ClassPathResource dataReopsitorySchema;
+
+           @Bean
+           DataSourceInitializer datasourceInitializer(DataSource dataSource) {
+               ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+
+               databasePopulator.addScript(dropReopsitoryTables);
+               databasePopulator.addScript(dataReopsitorySchema);
+               databasePopulator.setIgnoreFailedDrops(true);
+
+               DataSourceInitializer initializer = new DataSourceInitializer();
+               initializer.setDataSource(dataSource);
+               initializer.setDatabasePopulator(databasePopulator);
+
+               return initializer;
+           }
+
+           @Bean
+           public DataSource dataSource() {
+               EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+               return builder.setType(EmbeddedDatabaseType.H2).build();
+           }
+
+           @Bean
+           public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+               HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+               vendorAdapter.setDatabase(Database.H2);
+               vendorAdapter.setGenerateDdl(true);
+
+               LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+               factory.setJpaVendorAdapter(vendorAdapter);
+               factory.setPackagesToScan("internal.org.springframework.content.rest.support");
+               factory.setDataSource(dataSource());
+
+               return factory;
+           }
+
+           @Bean
+           public PlatformTransactionManager transactionManager() {
+               JpaTransactionManager txManager = new JpaTransactionManager();
+               txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+               return txManager;
+           }
+
+           @Bean
+           public FileSystemResourceLoader fileSystemResourceLoader() throws IOException {
+               return new FileSystemResourceLoader(Files.createTempDirectory("").toFile().getAbsolutePath());
+           }
+
+           @Bean
+           public ContentRestConfigurer configurer() {
+
+               return new ContentRestConfigurer() {
+                   @Override
+                   public void configure(RestConfiguration config) {
+                       config
+                           .cacheControl()
+                               .antMatcher("/testEntities/*", CacheControl.maxAge(Duration.ofSeconds(60)));
+                   }
+               };
+           }
+       }
+    }
+
+    @Test
+    public void noop() {
+    }
+}
