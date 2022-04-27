af9e7041766c39f62ee1388cde6d0b9f3598d79c
Paul Warren <paul_c_warren@yahoo.com>
2022-04-26 21:55:54 -0700

    Ensure elasticsearch indexer also has a rendition service if none is provided

--- spring-content-elasticsearch/src/main/java/internal/org/springframework/content/elasticsearch/ElasticsearchConfig.java
+++ spring-content-elasticsearch/src/main/java/internal/org/springframework/content/elasticsearch/ElasticsearchConfig.java
@@ -1,14 +1,19 @@
 package internal.org.springframework.content.elasticsearch;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.content.commons.renditions.RenditionProvider;
 import org.springframework.content.commons.renditions.RenditionService;
 import org.springframework.content.commons.search.IndexService;
 import org.springframework.content.elasticsearch.AttributeProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import internal.org.springframework.content.commons.renditions.RenditionServiceImpl;
 
 @Configuration
 public class ElasticsearchConfig {
@@ -22,9 +27,25 @@ public class ElasticsearchConfig {
     @Autowired(required = false)
     private AttributeProvider attributeProvider;
 
+    private List<RenditionProvider> providers = new ArrayList<>();
+
+    @Autowired(required=false)
+    public void setRenditionProviders(RenditionProvider... providers) {
+        for (RenditionProvider provider : providers) {
+            this.providers.add(provider);
+        }
+    }
+
+    public RenditionService getRenditionService() {
+        if (this.renditionService == null) {
+            this.renditionService = new RenditionServiceImpl(providers.toArray(new RenditionProvider[0]));
+        }
+        return this.renditionService;
+    }
+
     @Bean
     public IndexService elasticFulltextIndexService() throws IOException {
-        return new ElasticsearchIndexServiceImpl(client, renditionService, indexManager(), attributeProvider);
+        return new ElasticsearchIndexServiceImpl(client, this.getRenditionService(), indexManager(), attributeProvider);
     }
 
     @Bean
