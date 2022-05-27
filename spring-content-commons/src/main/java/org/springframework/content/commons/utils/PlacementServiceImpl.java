c8bc65efd3e501d4b7e4e89c8a198e6480eef818
Mikhail Kreymerman <mikhail-git@kreymerman.de>
2022-05-28 00:16:50 +0200

    Fix ContentTypeInfo generic parameters check; add checks for Converter parameters

--- spring-content-commons/src/main/java/org/springframework/content/commons/utils/PlacementServiceImpl.java
+++ spring-content-commons/src/main/java/org/springframework/content/commons/utils/PlacementServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.content.commons.mappingcontext.ContentPropertyInfoCon
 import org.springframework.core.ResolvableType;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.lang.Nullable;
 
 public class PlacementServiceImpl extends DefaultConversionService implements PlacementService {
 
@@ -22,17 +23,35 @@ public class PlacementServiceImpl extends DefaultConversionService implements Pl
         this.removeConvertible(Object.class, String.class);
     }
 
+    // from GenericConversionService::getRequiredTypeInfo
+    @Nullable
+    private ResolvableType[] getRequiredGenericParameters(Class<?> converterClass, Class<?> genericIfc) {
+        ResolvableType resolvableType = ResolvableType.forClass(converterClass).as(genericIfc);
+        ResolvableType[] generics = resolvableType.getGenerics();
+        if (generics.length < 2) {
+            return null;
+        }
+        Class<?> sourceType = generics[0].resolve();
+        Class<?> targetType = generics[1].resolve();
+        if (sourceType == null || targetType == null) {
+            return null;
+        }
+        return generics;
+    }
+
     @Override
     public void addConverter(Converter<?, ?> converter) {
-        // logic is similar to GenericConversionService.getRequiredTypeInfo
-        ResolvableType resolvableType = ResolvableType.forClass(converter.getClass()).as(Converter.class);
-        ResolvableType[] generics = resolvableType.getGenerics();
+        ResolvableType[] generics = getRequiredGenericParameters(converter.getClass(), Converter.class);
+        if (generics == null) {
+            throw new IllegalArgumentException("Unable to determine source type <S> and target type <T> for your " +
+                    "Converter [" + converter.getClass().getName() + "]; does the class parameterize those types?");
+        }
         ResolvableType sourceType = generics[0];
         if (sourceType.resolve() == ContentPropertyInfo.class) {
             ResolvableType targetType = generics[1];
 
             ResolvableType[] sourceTypeGenerics = sourceType.getGenerics();
-            if (generics.length != 2) {
+            if (sourceTypeGenerics.length != 2) {
                 throw new IllegalArgumentException(CONTENT_PROPERTY_INFO_GENERIC_PARAMETERS_MISSING_MESSAGE);
             }
 
