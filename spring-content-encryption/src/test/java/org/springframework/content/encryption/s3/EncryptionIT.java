973733e1ee40b58abed72f67b8de53efa13c6e11
Paul Warren <paul_c_warren@yahoo.com>
2022-12-06 22:58:30 -0800

    re-instate AWS_REGION env var

--- spring-content-encryption/src/test/java/org/springframework/content/encryption/s3/EncryptionIT.java
+++ spring-content-encryption/src/test/java/org/springframework/content/encryption/s3/EncryptionIT.java
@@ -66,6 +66,14 @@ import static org.junit.Assert.fail;
 @SpringBootTest(classes = EncryptionIT.Application.class, webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EncryptionIT {
 
+    static {
+        try {
+            setEnv(Collections.singletonMap("AWS_REGION", "us-west-1"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private static Object mutex = new Object();
 
     @Autowired
@@ -301,5 +309,33 @@ public class EncryptionIT {
         @ContentId private UUID contentId;
         @ContentLength private long contentLength;
         @MimeType private String contentMimeType;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void setEnv(Map<String, String> newenv) throws Exception {
+        try {
+            Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+            Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+            theEnvironmentField.setAccessible(true);
+            Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+            env.putAll(newenv);
+            Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+            theCaseInsensitiveEnvironmentField.setAccessible(true);
+            Map<String, String> cienv = (Map<String, String>)theCaseInsensitiveEnvironmentField.get(null);
+            cienv.putAll(newenv);
+        } catch (NoSuchFieldException e) {
+            Class[] classes = Collections.class.getDeclaredClasses();
+            Map<String, String> env = System.getenv();
+            for(Class cl : classes) {
+                if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+                    Field field = cl.getDeclaredField("m");
+                    field.setAccessible(true);
+                    Object obj = field.get(env);
+                    Map<String, String> map = (Map<String, String>) obj;
+                    map.clear();
+                    map.putAll(newenv);
+                }
+            }
+        }
     }
 }
