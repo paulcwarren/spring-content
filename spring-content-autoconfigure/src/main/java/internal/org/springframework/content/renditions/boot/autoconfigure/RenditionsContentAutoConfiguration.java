73abf046449a62ac43fce52596605504822f247f
Paul Warren <paul_c_warren@yahoo.com>
2023-03-29 06:16:36 +0000

    fix: re-enable renditions autoconfiguration (#1327)

--- spring-content-autoconfigure/src/main/java/internal/org/springframework/content/renditions/boot/autoconfigure/RenditionsContentAutoConfiguration.java
+++ spring-content-autoconfigure/src/main/java/internal/org/springframework/content/renditions/boot/autoconfigure/RenditionsContentAutoConfiguration.java
@@ -1,20 +1,20 @@
-//package internal.org.springframework.content.renditions.boot.autoconfigure;
-//
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-//import org.springframework.content.docx4j.config.Docx4jConfiguration;
-//import org.springframework.content.renditions.config.RenditionsConfiguration;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Import;
-//
-//@Configuration
-//@ConditionalOnClass(RenditionsConfiguration.class)
-//@Import(RenditionsConfiguration.class)
-//public class RenditionsContentAutoConfiguration {
-//
+package internal.org.springframework.content.renditions.boot.autoconfigure;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.content.renditions.config.RenditionsConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ConditionalOnClass(RenditionsConfiguration.class)
+@Import(RenditionsConfiguration.class)
+public class RenditionsContentAutoConfiguration {
+
+// temporarily disabled until we can upgrade docx4j to fix mac issues
 //    @Configuration
 //    @ConditionalOnClass(Docx4jConfiguration.class)
 //    @Import(Docx4jConfiguration.class)
 //    public static class Docx4jAutoConfiguration {
 //        //
 //    }
-//}
+}
