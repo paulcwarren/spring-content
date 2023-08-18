4433886f855404767282157a04d06e7a5df1d228
Paul Warren <paul_c_warren@yahoo.com>
2023-08-17 21:58:45 -0700

    fix: rest and store mappingcontext now use a common property naming convention
    Fixes 1536

--- spring-content-commons/src/main/java/org/springframework/content/commons/mappingcontext/ClassWalker.java
+++ spring-content-commons/src/main/java/org/springframework/content/commons/mappingcontext/ClassWalker.java
@@ -1,19 +1,16 @@
 package org.springframework.content.commons.mappingcontext;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
-import java.util.UUID;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.content.commons.utils.ContentPropertyUtils;
 import org.springframework.util.StringUtils;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ClassWalker {
 
@@ -23,6 +20,46 @@ public class ClassWalker {
 
     public ClassWalker(ClassVisitor visitor) {
         this.visitor = visitor;
+    }
+
+    public static String propertyName(String name) {
+        if (!StringUtils.hasLength(name)) {
+            return name;
+        }
+
+        String propertyName = calculateName(name);
+        if (propertyName != null) {
+            return propertyName;
+        }
+
+        String[] segments = split(name);
+        if (segments.length == 1) {
+            return segments[0];
+        }
+        else {
+            StringBuilder b = new StringBuilder();
+            for (int i=0; i < segments.length - 1; i++) {
+                b.append(segments[i]);
+            }
+            return b.toString();
+        }
+    }
+
+    public static String calculateName(String name) {
+        Pattern p = Pattern.compile("^(.+)(Id|Len|Length|MimeType|Mimetype|ContentType|(?<!Mime|Content)Type|(?<!Original)FileName|(?<!Original)Filename|OriginalFileName|OriginalFilename)$");
+        Matcher m = p.matcher(name);
+        if (m.matches() == false) {
+            return null;
+        }
+        return m.group(1);
+    }
+
+    public static String[] split(String name) {
+        if (!StringUtils.hasLength(name)) {
+            return new String[]{};
+        }
+
+        return name.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
     }
 
     public void accept(Class<?> klazz) {
